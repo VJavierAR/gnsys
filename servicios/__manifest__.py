3ba606eaa004bcb17727016ccb66fa9895b7d9d9
VJavierAR <javier.arrieta@documentointegral.mx>
2019-11-11 15:43:18 -0600

    comentario en manifest 4

--- servicios/__manifest__.py
+++ servicios/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Modulo que contiene cosas utiles para contadores
     """,
 
     'author': "My Company",
