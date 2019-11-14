0b9c16964bcb21532b830f86419f7dcaa23f1037
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-13 23:59:57 -0600

    ..

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -114,9 +114,9 @@ class helpdesk_update(models.Model):
         #terminar al rato 
         #[["x_studio_toner_compatible.id";,";=",x_studio_nombretmp]]"
         _logger.info("el id xD"+str(self.x_studio_nombretmp))
-        as=self.x_studio_nombretmp
+        g=str(self.x_studio_nombretmp)
         
-        list = ast.literal_eval(as)
+        list = ast.literal_eval(g)
         idf = self.team_id.id
         if idf == 8:
             _logger.info("el id xD Toner")
