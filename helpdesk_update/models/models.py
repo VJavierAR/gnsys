05b23888641cc7d8f60668929dcc9e27f73fdbb2
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-13 23:57:42 -0600

    error variable

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -114,8 +114,9 @@ class helpdesk_update(models.Model):
         #terminar al rato 
         #[["x_studio_toner_compatible.id";,";=",x_studio_nombretmp]]"
         _logger.info("el id xD"+str(self.x_studio_nombretmp))
-        ast=self.x_studio_nombretmp
-        list = ast.literal_eval(ast)
+        as=self.x_studio_nombretmp
+        
+        list = ast.literal_eval(as)
         idf = self.team_id.id
         if idf == 8:
             _logger.info("el id xD Toner")
