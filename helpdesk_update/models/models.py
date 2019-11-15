d1b7eb95b6a64cdffa22fcd0a478af6e458c299f
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-15 01:23:09 -0600

    e

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -29,15 +29,15 @@ class helpdesk_update(models.Model):
         idf = self.team_id.id
         if idf == 8:
             _logger.info("el id xD Toner"+g)            
-            res['domain']={'productosSolicitud':[('categ_id', '=', 5),('x_studio_toner_compatible.id','in',list)]}
+            return [('categ_id', '=', 5),('x_studio_toner_compatible.id','in',list)]
         if idf == 9:
             _logger.info("el id xD Reffacciones"+g)
-            res['domain']={'productosSolicitud':[('categ_id', '=', 7),('x_studio_toner_compatible.id','=',list[0])]}
+            return [('categ_id', '=', 7),('x_studio_toner_compatible.id','=',list[0])]
         #if idf != 9 and idf != 8:
         #    _logger.info("Compatibles xD"+g)
         #    res['domain']={'productosSolicitud':[('x_studio_toner_compatible.id','=',list[0])]}
-        _logger.info(" res :"+str(res))    
-        return res
+        #_logger.info(" res :"+str(res))    
+        #return res
 
     productosSolicitud = fields.Many2many('product.product', string="Productos Solicitados",domain=_productos_solicitud_filtro)
     
