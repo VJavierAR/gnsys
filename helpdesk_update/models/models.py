c3802982bbcf61568b9fd97977644fe565437d78
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-19 13:57:46 -0600

    quue tiene g

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -138,22 +138,22 @@ class helpdesk_update(models.Model):
     def productos_filtro(self):
         res = {}             
         g=str(self.x_studio_nombretmp)
-        list = ast.literal_eval(g)
         
+        raise exceptions.ValidationError("test " + str(g))        
+
+        list = ast.literal_eval(g)        
         idf = self.team_id.id
         tam = len(list)
-        raise exceptions.ValidationError("test " + str(tam))
-        if  tam > 0:
-            if idf == 8:
-                _logger.info("el id xD Toner"+g)            
-                res['domain']={'x_studio_productos':[('categ_id', '=', 5),('x_studio_toner_compatible.id','in',list)]}
-            if idf == 9:
-                _logger.info("el id xD Reffacciones"+g)
-                res['domain']={'x_studio_productos':[('categ_id', '=', 7),('x_studio_toner_compatible.id','=',list[0])]}
-            if idf != 9 and idf != 8:
-                _logger.info("Compatibles xD"+g)
-                res['domain']={'x_studio_productos':[('x_studio_toner_compatible.id','=',list[0])]}
-            _logger.info("res"+str(res))
+        if idf == 8:
+           _logger.info("el id xD Toner"+g)            
+           res['domain']={'x_studio_productos':[('categ_id', '=', 5),('x_studio_toner_compatible.id','in',list)]}
+        if idf == 9:
+           _logger.info("el id xD Reffacciones"+g)
+           res['domain']={'x_studio_productos':[('categ_id', '=', 7),('x_studio_toner_compatible.id','=',list[0])]}
+        if idf != 9 and idf != 8:
+           _logger.info("Compatibles xD"+g)
+           res['domain']={'x_studio_productos':[('x_studio_toner_compatible.id','=',list[0])]}
+           _logger.info("res"+str(res))
         return res
         
     @api.onchange('x_studio_zona')
