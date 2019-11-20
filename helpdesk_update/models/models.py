2970a5f38308b69031790da61757118a66db7c0d
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-19 18:04:40 -0600

    sudo x2

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -70,14 +70,14 @@ class helpdesk_update(models.Model):
                 #self.env.cr.commit()
                 #for c in record.x_studio_field_tLWzF:
                 for c in record.x_studio_productos:
-                    self.env['sale.order.line'].create({'order_id' : sale.id
+                    self.sudo()..env['sale.order.line'].create({'order_id' : sale.id
                                                       , 'product_id' : c.id
                                                       , 'product_uom_qty' : c.x_studio_cantidad_pedida
                                                       })
                 record['x_studio_field_nO7Xg'] = sale.id
                 sale.env['sale.order'].write({'x_studio_tipo_de_solicitud' : 'Venta'})
                 #self.env.invalidate_all()
-                self.env.cr.execute("update sale_order set x_studio_tipo_de_solicitud = 'Venta' where  id = " + str(sale.id) + ";")
+                self.sudo()..env.cr.execute("update sale_order set x_studio_tipo_de_solicitud = 'Venta' where  id = " + str(sale.id) + ";")
                 #self.env.cr.commit()
     
     @api.onchange('x_studio_verificacin_de_refaccin')
