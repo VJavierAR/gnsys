bc8e545581b43dbeb55da2b7f2e0c95a0ef5e56d
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-21 13:15:36 -0600

    tools

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import _, models, fields, api
+from odoo import _, models, fields, api, tools
 from email.utils import formataddr
 from odoo.exceptions import UserError
 from odoo import exceptions
