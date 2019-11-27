d005b2296cb8e6222df1d2c41fc1501d47c5bc73
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-11-27 14:53:57 -0600

    roll

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -764,7 +764,7 @@ class helpdesk_update(models.Model):
         for record in self:
            # cantidad_numeros_serie = record.x_studio_tamao_lista
            # _logger.info("******************team_id: "+ str(record.team_id.id) + " cantidad_numeros_serie: "+ str(cantidad_numeros_serie))
-           # if  int(cantidad_numeros_serie) < 2 :
+            if  int(cantidad_numeros_serie) < 2 :
                 _logger.info('record_ 1: ' + str(self._origin.partner_id))
                 _logger.info('record_id 1: ' + str(self._origin.id))
                 _my_object = self.env['helpdesk.ticket']
@@ -876,8 +876,8 @@ class helpdesk_update(models.Model):
             #return res
             #return {'value': v}
         
-        #else:
-        #    raise exceptions.ValidationError("No es posible registrar más de un número de serie")
+        else:
+            raise exceptions.ValidationError("No es posible registrar más de un número de serie")
     
 
                 
