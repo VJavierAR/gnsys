8dd204fcccc83c0f1cdd5d41e0e9e1ca6881852f
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-12-02 10:30:50 -0600

    s

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -182,8 +182,8 @@ class helpdesk_update(models.Model):
     
     @api.onchange('stage_id')
     def actualiza_datos_estado(self):
-        _logger.info("staged()  **********************************#*"+str(self._uid.name))
-        self.env['x_historial_helpdesk'].create({'x_id_ticket':self.x_studio_id_ticket ,'x_persona': self._uid.name,'x_estado': self.stage_id.name})        
+        _logger.info("staged()  **********************************#*"+str(self.env.user.name))
+        self.env['x_historial_helpdesk'].create({'x_id_ticket':self.x_studio_id_ticket ,'x_persona': self._uid.name,'x_estado': self.env.user.name})        
     
     
     
