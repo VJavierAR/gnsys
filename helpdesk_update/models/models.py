21b7cd3e2fa0f4804204326869b2334a1f7f7935
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-12-05 14:22:18 -0600

    asfafdf

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -46,7 +46,7 @@ class helpdesk_update(models.Model):
 
     productosSolicitud = fields.Many2many('product.product', string="Productos Solicitados",domain=_productos_solicitud_filtro)
     """
-    
+    """
     @api.one
     @api.depends('x_studio_equipo_por_nmero_de_serie.x_studio_contador_color')
     def genera_registro_contadores(self):
@@ -54,7 +54,7 @@ class helpdesk_update(models.Model):
         for record in self:
             contadorColor = record.x_studio_equipo_por_nmero_de_serie.x_studio_contador_color
             raise exceptions.ValidationError(str(contadorColor))
-         
+    """     
             
             
     
