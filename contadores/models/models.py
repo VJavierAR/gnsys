390e7dac7b9d6b8ce415deaaea7ca12333115652
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-08 10:16:04 -0600

    sadad

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -59,7 +59,7 @@ class contadores(models.Model):
         res = {}
         d=[]
         if(self.cliente):
-            lotes=self.env['stock.production.lot'].search([['x_studio_ultima_ubicacin', '=' ,self.cliente.name]])
+            lotes=self.env['stock.production.lot'].search([['x_studio_ubicaciontest', '=' ,self.cliente.name]])
             _logger.info("Contadores"+str(len(lotes)))
             for l in lotes:
                 #if(l.x_studio_ultima_ubicacin == self.cliente.name):
