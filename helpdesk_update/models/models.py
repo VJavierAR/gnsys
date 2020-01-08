e483498d387cad9fed3f708ec6046a6358397b41
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-08 17:51:45 -0600

    hjklñ

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -112,7 +112,7 @@ class helpdesk_update(models.Model):
                 ss = self.env.cr.execute(query)             
                 _logger.info("**********fun: asignacion(), estado: " + str(self.stage_id.name))                
                 self.env['x_historial_helpdesk'].create({'x_id_ticket':self.x_studio_id_ticket ,'x_persona':self.env.user.name ,'x_estado': "Asignado"})
-        """
+        
         res = {}
         idEquipoDeAsistencia = self.team_id.id
         query = "select * from helpdesk_team_res_users_rel where helpdesk_team_id = " + str(idEquipoDeAsistencia) + ";"
@@ -125,10 +125,10 @@ class helpdesk_update(models.Model):
             _logger.info("*********idUsuario: " + str(idUsuario))
             listaUsuarios.append(idUsuario[1])
         _logger.info(str(listaUsuarios))
-        dominio = [('res_users.id', 'in', listaUsuarios)]
+        dominio = [('id', 'in', listaUsuarios)]
         res['domain'] = {'user_id': dominio}
         return res
-        """
+        
         
     
     
