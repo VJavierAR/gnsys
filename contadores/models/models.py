76c3ee4464ea4b26362ed64370fc17db770b41e2
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-08 10:00:57 -0600

    asdfsa

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -56,14 +56,14 @@ class contadores(models.Model):
         res = {}
         d=[]
         if(self.cliente):
-            lotes=self.env['stock.production.lot'].search([])
+            lotes=self.env['stock.production.lot'].search([['x_studio_ultima_ubicacin', '=' ,self.cliente.name]])
             for l in lotes:
-                if(l.x_studio_ultima_ubicacin == self.cliente.name):
-                    datos={}
-                    datos['serie']=l.id
-                    d.append(datos)            
+                #if(l.x_studio_ultima_ubicacin == self.cliente.name):
+                datos={}
+                datos['serie']=l.id
+                d.append(datos)            
             self.order_line=d
-        return res
+        #return res
     
     
 
