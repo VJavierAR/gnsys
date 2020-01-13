2eed1d6fae478750c2de48ebeab20838dcdc1ae8
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-13 08:27:22 -0600

    cerrar ticket

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -1019,12 +1019,12 @@ class helpdesk_update(models.Model):
            query = "select * from helpdesk_ticket_stock_production_lot_rel where stock_production_lot_id  = " + str(self.x_studio_equipo_por_nmero_de_serie[0].id) + "limit 1 ;"
            self.env.cr.execute(query)
            informacion = self.env.cr.fetchall()
-           queryD = "select stage_id from helpdesk_ticket where id  = " + str(informacion[0][0]) + "limit 1;"
+           queryD = "select stage_id,id from helpdesk_ticket where id  = " + str(informacion[0][0]) + "limit 1;"
            self.env.cr.execute(queryD)
            informacionD = self.env.cr.fetchall()                                    
            _logger.info("actualiza_datos_cliente2()  "+str(self.x_studio_equipo_por_nmero_de_serie[0].id) +'18=='+ str(informacionD[0][0]))
-           if int(self.x_studio_equipo_por_nmero_de_serie[0].id)==int(informacion[0][1]):
-              raise exceptions.ValidationError("No es posible registrar número de serie ")
+           if int(self.x_studio_equipo_por_nmero_de_serie[0].id)==int(informacion[0][1] and (informacionD[0][0]) == 18):
+              raise exceptions.ValidationError("No es posible registrar número de serie, primero cerrar el ticket con el id  "+str(informacionD[0][1]))
 
         
         _logger.info("self._origin: " + str(self._origin) + ' self._origin.id: ' + str(self._origin.id))
