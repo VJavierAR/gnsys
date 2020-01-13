c8353340195fffbf4ad4c39dbdc0e1a646670441
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-13 08:40:22 -0600

    a

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -1022,7 +1022,7 @@ class helpdesk_update(models.Model):
            queryD = "select stage_id,id from helpdesk_ticket where id  = " + str(informacion[0][0]) + "limit 1;"
            self.env.cr.execute(queryD)
            informacionD = self.env.cr.fetchall()
-           _logger.info("actualiza_datos_cliente2()  "+str(informacionD) )
+           _logger.info("actualiza_datos_cliente2()  "+str(informacionD) +'  '+ str(informacion))
            _logger.info("actualiza_datos_cliente3()  "+str(self.x_studio_equipo_por_nmero_de_serie[0].id) +'18=='+ str(informacionD[0][0]))
            if int(self.x_studio_equipo_por_nmero_de_serie[0].id)==int(informacion[0][1] and int(informacionD[0][0]) == 18):
               raise exceptions.ValidationError("No es posible registrar número de serie, primero cerrar el ticket con el id  "+str(informacionD[0][1]))
