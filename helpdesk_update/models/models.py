0ab2c64bda1c3f5b9807edcb0fa52cbfb2458432
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-16 15:12:18 -0600

    updatre

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -77,7 +77,7 @@ class helpdesk_update(models.Model):
     def cambiar_direccion_entrega(self):
         _logger.info("***************Que tiene en pedido de venta: " + str(self.x_studio_field_nO7Xg))
         sale = self.x_studio_field_nO7Xg
-        if self.x_studio_field_nO7Xg != False and self.x_studio_empresas_relacionadas and self.x_studio_field_nO7Xg.state != 'sale':
+        if self.x_studio_field_nO7Xg != False and self.x_studio_empresas_relacionadas != False and self.x_studio_field_nO7Xg.state != 'sale':
             _logger.info("****************solicitud: " + str(self.x_studio_field_nO7Xg.id))
             _logger.info("****************localidad: " + str(self.x_studio_empresas_relacionadas.id))
             #self.env['sale.order'].write(['partner_shipping_id','=',''])
