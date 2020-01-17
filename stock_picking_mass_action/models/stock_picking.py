6f21902be85716cd83ca33ea6ad0c3508390e9af
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-17 11:00:58 -0600

    asdfgh

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -12,6 +12,21 @@ class StockPicking(Model):
     #ticketOrigenEnVenta = fields.Char(string='Documento de origen en venta', store=True, related='sale_id.origin')
     estado = fields.Text(compute = 'x_historial_ticket_actualiza')
     lineTemp=fields.One2many('stock.pick.temp','picking')
+    state = fields.Selection([
+    ('draft', 'Draft'),
+    ('waiting', 'Waiting Another Operation'),
+    ('confirmed', 'Waiting'),
+    ('assigned', 'Ready'),
+    ('done', 'Validado'),('distribucion', 'Distribución'),('cancel', 'Cancelled'),
+], string='Status', compute='_compute_state',
+    copy=False, index=True, readonly=True, store=True, track_visibility='onchange',
+    help=" * Draft: not confirmed yet and will not be scheduled until confirmed.\n"
+         " * Waiting Another Operation: waiting for another move to proceed before it becomes automatically available (e.g. in Make-To-Order flows).\n"
+         " * Waiting: if it is not ready to be sent because the required products could not be reserved.\n"
+         " * Ready: products are reserved and ready to be sent. If the shipping policy is 'As soon as possible' this happens as soon as anything is reserved.\n"
+         " * Done: has been processed, can't be modified or cancelled anymore.\n"
+         " * Cancelled: has been cancelled, can't be confirmed anymore.")
+
     
     @api.multi
     @api.depends('state')
