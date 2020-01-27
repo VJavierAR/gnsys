ec8f12f8cf9bf4b1b69c227a30044cf99c36ed6d
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-27 14:06:57 -0600

    erfe

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -900,7 +900,7 @@ class helpdesk_update(models.Model):
                 record['x_studio_field_nO7Xg'] = sale.id
                 _logger.info("-----------------------------------------sale.id: " + str(sale.id))
                 for c in record.x_studio_equipo_por_nmero_de_serie:
-                    pro=self.env['product.product'].search([['name','=',c.x_studio_toner_compatible.name],['categ_id','=',5]])
+                    pro=self.env['product.product'].search([['name','ilike',c.x_studio_toner_compatible.name.replace(' ','').replace('-','')],['categ_id','=',5]])
                     gen=pro.sorted(key='qty_available',reverse=True)
                     datos={'name': ' '
                            ,'order_id' : sale.id
