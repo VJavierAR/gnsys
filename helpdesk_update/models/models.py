441240a91045e5cb3d362e6831845df7cfa0d1d1
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-01-27 15:01:14 -0600

    Update linea 904

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -901,6 +901,7 @@ class helpdesk_update(models.Model):
                 _logger.info("-----------------------------------------sale.id: " + str(sale.id))
                 for c in record.x_studio_equipo_por_nmero_de_serie:
                     _logger.info("-------------------------c.id: " + str(c.id) + " c.name: " + str(c.name))
+                    _logger.info("-------------------------c.x_studio_toner_compatible.id: " + str(c.x_studio_toner_compatible.id))
                     _logger.info("-------------------------c.x_studio_toner_compatible.name: " + str(c.x_studio_toner_compatible.name))
                     pro=self.env['product.product'].search([['name','ilike',c.x_studio_toner_compatible.name.replace(' ','').replace('-','')],['categ_id','=',5]])
                     gen=pro.sorted(key='qty_available',reverse=True)
