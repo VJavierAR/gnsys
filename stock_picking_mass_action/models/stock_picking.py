f74306d336aad40da7f85d5a5d1d0a8d7fe15f13
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-18 17:13:36 -0600

    a

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -18,7 +18,7 @@ class StockPicking(Model):
     value2 = fields.Integer(store=True)
     lineasBack = fields.One2many(related='backorder_ids.move_ids_without_package')
     ruta_id=fields.Many2one('creacion.ruta')
-    documentosDistro = fields.Many2many('ir.attachment', string="Evidencias ")
+    #documentosDistro = fields.Many2many('ir.attachment', string="Evidencias ")
     #historialTicket = fields.One2many('ir.attachment','res_id',string='Evidencias al ticket',store=True,track_visibility='onchange')
 
     @api.onchange('carrier_tracking_ref')
