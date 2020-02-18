9f1717748ae69fdca932846b4420453053f118e4
cesar <kliko1989@gmail.com>
2020-02-18 01:57:26 -0600

    Update ruta.py
    sssssssss

--- stock_picking_mass_action/models/ruta.py
+++ stock_picking_mass_action/models/ruta.py
@@ -5,10 +5,21 @@ from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 import logging, ast
 _logger = logging.getLogger(__name__)
 
-class StockPicking(Model):
+class CreacionRuta(Model):
 	_name='creacion.ruta'
 	name=fields.Char()
 	chofer=fields.Many2one('hr.employee')
 	vehiculo=fields.Many2one('fleet.vehicle')
 	ordenes=fields.Many2many('stock.picking')
 	zona=fields.Selection([["SUR","SUR"],["NORTE","NORTE"],["PONIENTE","PONIENTE"],["ORIENTE","ORIENTE"],["CENTRO","CENTRO"],["DISTRIBUIDOR","DISTRIBUIDOR"],["MONTERREY","MONTERREY"],["CUERNAVACA","CUERNAVACA"],["GUADALAJARA","GUADALAJARA"],["QUERETARO","QUERETARO"],["CANCUN","CANCUN"],["VERACRUZ","VERACRUZ"],["PUEBLA","PUEBLA"],["TOLUCA","TOLUCA"],["LEON","LEON"],["COMODIN","COMODIN"],["VILLAHERMOSA","VILLAHERMOSA"],["MERIDA","MERIDA"],["VERACRUZ","VERACRUZ"],["ALTAMIRA","ALTAMIRA"]])
+	estado=fields.Selection([["borrador","Borrador"],["valido","Confirmado"]])
+
+	def confirmar(self):
+		self.estado="valido"
+
+	@api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('ruta') or _('New')
+        result = super(CreacionRuta, self).create(vals)
+        return result
+
