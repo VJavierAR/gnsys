4ac07792217a9a1a83e4067e4ac906ab4bf57f73
cesar <kliko1989@gmail.com>
2020-02-21 01:38:13 -0600

    fghjkl

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -127,7 +127,7 @@ class StockPicking(Model):
     
     
     @api.multi
-    @api.depends('state','ajusta')
+    @api.depends('state')
     def x_historial_ticket_actualiza(self):
         for record in self:
             if(record.backorder==False):
