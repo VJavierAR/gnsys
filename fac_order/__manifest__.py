fe7484d7d92ef8ef5f711be1001af60ece7935ae
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-25 02:47:48 -0600

    a

--- fac_order/__manifest__.py
+++ fac_order/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '12.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale'],
@@ -29,7 +29,10 @@
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
+     'installable': True,
+    'application': True,
+    'auto_install': False,
 }
