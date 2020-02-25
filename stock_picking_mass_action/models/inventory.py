628d8120bd75f1ae6099ed30aa3f4549cfe2d1b7
cesar <kliko1989@gmail.com>
2020-02-25 16:55:01 -0600

    dsad

--- stock_picking_mass_action/models/inventory.py
+++ stock_picking_mass_action/models/inventory.py
@@ -9,12 +9,12 @@ class StockPicking(Model):
     _inherit = 'stock.inventory'    
 
     def _action_done(self):
-    negative = next((line for line in self.mapped('line_ids') if line.product_qty < 0 and line.product_qty != line.theoretical_qty), False)
-    if negative:
-        raise UserError(_('You cannot set a negative product quantity in an inventory line:\n\t%s - qty: %s') % (negative.product_id.name, negative.product_qty))
-    self.action_check()
-    self.write({'state': 'done'})
-    self.post_inventory()
-    for r in self.mapped('line_ids'):
-        log(str(r.id), level='info')
-    return True
+        negative = next((line for line in self.mapped('line_ids') if line.product_qty < 0 and line.product_qty != line.theoretical_qty), False)
+        if negative:
+            raise UserError(_('You cannot set a negative product quantity in an inventory line:\n\t%s - qty: %s') % (negative.product_id.name, negative.product_qty))
+        self.action_check()
+        self.write({'state': 'done'})
+        self.post_inventory()
+        for r in self.mapped('line_ids'):
+            log(str(r.id), level='info')
+        return True
