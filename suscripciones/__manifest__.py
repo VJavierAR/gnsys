5437653e03b6f33a056db4533c484aced4c2b954
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-25 03:20:34 -0600

    b

--- suscripciones/__manifest__.py
+++ suscripciones/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management'],
+    'depends': ['base','sale_management' ],
 #va de nuevo
     # always loaded
     'data': [
