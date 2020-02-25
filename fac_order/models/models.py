b80bdeff3dadbc782b4788b71386382ba691e5d3
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-25 17:33:19 -0600

    Update sale.order.detalle linea 69 a 116

--- fac_order/models/models.py
+++ fac_order/models/models.py
@@ -86,44 +86,31 @@ class detalle(models.Model):
       lecturaAnteriorColor = fields.Integer(string='Lectura anterior color')
       paginasProcesadasColor = fields.Integer(string='Páginas procesadas color')
       
+      periodo = fields.Text(string="Periodo")
       
-      """
+      
       @api.multi 
-      def llamado_boton(self):
+      def probar(self):
         for r in self:                    
           f=len(r.x_studio_servicios_contratos)
           ff=r.x_studio_servicios_contratos
           if f>0:
             h=[]
-            g=[]
             p=[]
             for m in ff:
               h.append(m.id)
-            g=self.env['sale.subscription.line'].search([('analytic_account_id', '=', int(h[0]))])
-            for s in g:
-              pp=s.product_id.name
-              if pp=='Clic excedente monocromático':    
-                 eBN=s.price_unit
-              if pp=='Clic excedente color':    
-                 eColor=s.price_unit
-              if pp=='Clic monocromática':
-                 bolsabn=s.quantity  
-              if pp=='Clic color':
-                 bolsabn=s.quantity               
-               #eBN=g.
-               #bolsabn=g[0].
-               #bolsacolor=g[0].
             p=self.env['stock.production.lot'].search([('x_studio_suscripcion', '=', int(h[0]))])
             sale=self.env['sale.order'].search([('name', '=', self.name)])
-            #for t in g:
-            #    self.env['sale.order.line'].create({'order_id': sale.id,'product_id':t.product_id.id})
             for h in p:
-                if h.x_studio_color_bn=='B/N':  
-                   self.env['sale.order.line'].create({'order_id': sale.id,'product_id':h.product_id.id,'x_studio_field_9nQhR':h.id,'product_uom_qty':h.x_studio_pg_proc,'price_unit':eBN})            
-                if h.x_studio_color_bn=='Color':  
-                   self.env['sale.order.line'].create({'order_id': sale.id,'product_id':h.product_id.id,'x_studio_field_9nQhR':h.id,'product_uom_qty':h.x_studio_pg_proc_color,'price_unit':eColor})
-                   self.env['sale.order.line'].create({'order_id': sale.id,'product_id':h.product_id.id,'x_studio_field_9nQhR':h.id,'product_uom_qty':h.x_studio_pg_proc,'price_unit':eBN})                 
-      """
-      
-      
-      
+                  self.env['sale.order.detalle'].create({'saleOrder': sale.id
+                                                         , 'producto': h.product_id.display_name
+                                                         , 'serieEquipo': h.name
+                                                         
+                                                         , 'ultimaLecturaBN': h.x_studio_ultimalecturam
+                                                         , 'lecturaAnteriorBN': h.x_studio_lec_ant_bn
+                                                         , 'paginasProcesadasBN': h.x_studio_pg_proc
+                                                         
+                                                         , 'ultimaLecturaColor': h.x_studio_ultimalecturacolor
+                                                         , 'lecturaAnteriorColor': h.x_studio_lec_ant_color
+                                                         , 'paginasProcesadasColor': h.x_studio_pg_proc_color
+                                                        })            
