887149be411e0619c4d3c5cdaccab7d7a847d3ca
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-25 02:22:36 -0600

    fac

--- fac_order/models/models.py
+++ fac_order/models/models.py
@@ -8,6 +8,33 @@ class fac_order(models.Model):
 #     _name = 'fac_order.fac_order'
 
       nameDos = fields.Char()
+      
+     @api.multi 
+     def facturar(self):
+          for r in self:
+          f=len(r.x_studio_servicios_contratos)
+          ff=r.x_studio_servicios_contratos
+          if f>0:
+            h=[]
+            g=[]
+            p=[]
+            for m in ff:
+              h.append(m.id)
+            g=self.env['sale.subscription.line'].search([('analytic_account_id', '=', int(h[0]))])
+            p=self.env['stock.production.lot'].search([('x_studio_suscripcion', '=', int(h[0]))])
+            sale=self.env['sale.order'].search([('name', '=', self.name)])
+            #r['x_studio_llenado_de_info_xd']=str(p)+"olo"
+            for t in g:
+                self.env['sale.order.line'].create({'order_id': sale.id,'product_id':t.product_id.id})
+            for h in p:
+                self.env['sale.order.line'].create({'order_id': sale.id,'product_id':h.product_id.id})
+
+            
+      
+      
+      
+      
+      
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
