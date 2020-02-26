64121483a9a0ee5cf8031c8e60a982b1a5906044
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-26 11:49:00 -0600

    asasasasas

--- fac_order/models/models.py
+++ fac_order/models/models.py
@@ -76,11 +76,11 @@ class fac_order(models.Model):
                 #raise exceptions.ValidationError( str(g)+" "+str(serUNO)+" , "+str(sale.id)+",  "+ str(procesadasColorBN)+",  "+str(eBN))
             if bolsabn<procesadasColorBN:
                self.env['sale.order.line'].create({'order_id': sale.id,'product_id':serUNO,'product_uom_qty':0.0,'price_unit':eBN,'x_studio_bolsa':bolsabn})
-            if procesadasColorBN>bolsabn:
+            if bolsabn>procesadasColorBN:
                self.env['sale.order.line'].create({'order_id': sale.id,'product_id':serUNO,'product_uom_qty':procesadasColorBN-bolsabn,'price_unit':eBN,'x_studio_bolsa':bolsabn})
             if bolsacolor<procesadasColorTotal:            
                self.env['sale.order.line'].create({'order_id': sale.id,'product_id':serDOS,'product_uom_qty':0.0,'price_unit':eColor,'x_studio_bolsa':bolsacolor})
-            if procesadasColorTotal>bolsacolor:
+            if bolsacolor>procesadasColorTotal:
                self.env['sale.order.line'].create({'order_id': sale.id,'product_id':serDOS,'product_uom_qty':procesadasColorTotal-bolsacolor,'price_unit':eColor,'x_studio_bolsa':bolsacolor})   
 
             
