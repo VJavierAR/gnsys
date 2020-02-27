8be2ef42cb3983c4df9976f739937ab082751742
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-02-27 17:12:58 -0600

    Creando modelo servicios

---
+++ servicios_gnsys/models/models.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, models, fields, api, tools
+from email.utils import formataddr
+from odoo.exceptions import UserError
+from odoo import exceptions, _
+import logging, ast
+import datetime, time
+_logger = logging.getLogger(__name__)
+
+class servicios_gnsys(models.Model):
+    _name = 'servicios'
+    _description = 'Servicios GNSYS'
+
+
+    productos = fields.One2many('product.product', string="Productos")
+    descripcion = fields.Text(string="Descripción")
+    rentaMensual = fields.Text(string="Renta mensual")
+
+    bolsaBN = fields.Integer(string="Bolsa B/N")
+    clickExcedenteBN = fields.Integer(string="Click excedente B/N")
+    procesadoBN = fields.Integer(string="Procesado B/N")
+
+    bolsaColor = fields.Integer(string="Bolsa color")
+    clickExcedenteColor = fields.Integer(string="Click excedente color")
+    procesadoColor = fields.Integer(string="Procesado color")
+    
+    
+    series = fields.One2many('stock.production.lot', string="Series")
+    
+    color_bn = fields.Integer(string="Color - B/N")
+
+    lecAntBN = fields.Integer(string="Lectura anterior B/N")
+    lecActualBN = fields.Integer(string="Lectura actual B/N")
+    procesadoBN = fields.Integer(string="Procesado B/N")
+
+    lecAntColor = fields.Integer(string="Lectura anterior color")
+    lecActualColor = fields.Integer(string="Lectura actual color")
+    procesadoColor = fields.Integer(string="Procesado color")
+
+    modelo = fields.Text(string="Modelo")
