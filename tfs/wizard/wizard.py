d131e8ce6d72a8dc72fb2a3a4cd4fca8b40f6355
cesar <kliko1989@gmail.com>
2020-03-02 12:22:28 -0600

    otra

--- tfs/wizard/wizard.py
+++ tfs/wizard/wizard.py
@@ -19,7 +19,7 @@ class StockImmediateTransfer(models.TransientModel):
 
     def crear_ticket(self):
     	for r in self.tfs_ids:
-    		self.env['helpdesk.ticket'].create({'name':'Falla en rendimiento <60% '+'Serie: '+r.serie,'x_studio_equipo_por_nmero_de_serie':[(4,r.serie.id)]})
+    		self.env['helpdesk.ticket'].create({'name':'Falla en rendimiento <60% '+'Serie: '+r.serie.name,'x_studio_equipo_por_nmero_de_serie':[(4,r.serie.id)]})
     		In=r.inventario.search([['product_id.name','=',r.producto.name],['location_id','=',r.almacen.lot_stock_id.id]]).sorted(key='quantity',reverse=True)
     		if(len(In)>0):
 		    	In[0].write({'quantity':In[0].quantity-1})
