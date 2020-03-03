ab1fc8f31859080c0ae23e886c194aebba7aefc8
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-03 11:00:17 -0600

    UPDATE GASTOS

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -16,22 +16,22 @@ class gastos_gnsys(models.Model):
     quienesAutorizan = fields.One2many('hr.employee' , string = "Quien (es) autorizan",track_visibility='onchange')
     quienesReciben   = fields.One2many('hr.employee' , string = "Quien (es) reciben",track_visibility='onchange')
 
-    montoAprobado    = fields.float(string = 'Monto aprobado',track_visibility='onchange')
-    montoAtnticipado = fields.float(string = 'Monto anticipo',track_visibility='onchange')
+    montoAprobado    = fields.Float(string = 'Monto aprobado',track_visibility='onchange')
+    montoAtnticipado = fields.Float(string = 'Monto anticipo',track_visibility='onchange')
 
 
-    formaDepago      = fields.selection((('Efectivo','Efectivo'), ('Cheque','Cheque'),('Depósito','Depósito'),('Transferencia','Transferencia')), string = "Forma de pago (Receptor)",track_visibility='onchange')
+    formaDepago      = fields.Selection((('Efectivo','Efectivo'), ('Cheque','Cheque'),('Depósito','Depósito'),('Transferencia','Transferencia')), string = "Forma de pago (Receptor)",track_visibility='onchange')
 
 
-    comoAplicaContablemente     = fields.selection((('Opcion','Opcion'),('Opcion','Opcion'),('Opcion','Opcion')), string = "Como aplica contablemente",track_visibility='onchange')
-    porCubrirAnticipo           = fields.datetime(string = 'Fecha límite de pago',track_visibility='onchange')
+    comoAplicaContablemente     = fields.Selection((('Opcion','Opcion'),('Opcion','Opcion'),('Opcion','Opcion')), string = "Como aplica contablemente",track_visibility='onchange')
+    porCubrirAnticipo           = fields.Datetime(string = 'Fecha límite de pago',track_visibility='onchange')
 
-    fechaPago                   = fields.datetime(string = 'Fecha de pago',track_visibility='onchange')
+    fechaPago                   = fields.Datetime(string = 'Fecha de pago',track_visibility='onchange')
 
-    fechaLimiteDeComprobacion   = fields.datetime(string = 'Fecha límite de comprobación',track_visibility='onchange')
+    fechaLimiteDeComprobacion   = fields.Datetime(string = 'Fecha límite de comprobación',track_visibility='onchange')
 
 
-    anticipoCubierto            = fields.float(string = 'Anticipo cubierto',track_visibility='onchange')
+    anticipoCubierto            = fields.Float(string = 'Anticipo cubierto',track_visibility='onchange')
 
     quienValida                 = fields.One2many('hr.employee', 'gastoValida', string = "Validado por",track_visibility='onchange')
 
@@ -44,7 +44,7 @@ class motivos_gastos(models.Model):
 
     gasto  = fields.Many2one('gatos', string = "Gasto ", track_visibility='onchange')
     motivoDescripcion = fields.Text()
-    tipoDeMotivo      = fields.selection((('!','1'), ('2','2')), string = "Tipo de motivo",track_visibility='onchange')
+    tipoDeMotivo      = fields.Selection((('!','1'), ('2','2')), string = "Tipo de motivo",track_visibility='onchange')
 
 class empleados_gastos(models.Model):
     _inherit = 'hr.employee'
