54f9bd7eb35552930eb89c7ef37116e438c3ae3a
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-03 15:05:07 -0600

    Update gastos

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -36,6 +36,48 @@ class gastos_gnsys(models.Model):
     quienValida                 = fields.One2many('hr.employee', 'gastoValida', string = "Validado por",track_visibility='onchange')
 
     motivos                     = fields.One2many('motivos', 'gasto', string = "Motivos",track_visibility='onchange')
+    
+    #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    comproboCorrectamente       = fields.Selection((('Exacto','Exacto'),('Parcial','Parcial'),('Excedido','Excedido')), string = "Tipo de comprobación",track_visibility='onchange')
+
+    requiereDevolucion          = fields.Selection((('Efectivo','Efectivo'),('Descuento nómina','Descuento nómina')), string = "¿Requiere devolución?",track_visibility='onchange')
+
+    #En caso de que sea por efectivo o excacto
+    #Parcial
+    monto                       = fields.Float(string = 'Monto a cubrir',track_visibility='onchange')
+    #En caso de que sea por efectivo o por nomina
+    comoAplicaContablementeRequerimeintoDevolucion  = fields.Selection((('Opcion','Opcion'),('Opcion','Opcion'),('Opcion','Opcion')), string = "Como aplica contablemente",track_visibility='onchange')
+
+
+    #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    #En caso de que la devolucion sea excendida
+
+    formaDepago = fields.Selection((('¿La empresa cubre adicional? ¿Cuánto?','¿La empresa cubre adicional? ¿Cuánto?'), ('¿Receptor cubre adicional?','¿Receptor cubre adicional?')), string = "Forma de pago (Receptor)",track_visibility='onchange')
+    #La empresa cubre lo adicional ¿La empresa cubre adicional? ¿Cuánto?
+    #Forma en que caso de que la empresa cubra lo adicional
+    formaDepagoExtendida    = fields.Selection((('Efectivo','Efectivo'), ('Cheque','Cheque'),('Depósito','Depósito'),('Transferencia','Transferencia')), string = "Forma de pago (Receptor)",track_visibility='onchange')
+
+    fechaLimiteDePago       = fields.Datetime(string = 'Fecha límite de pago',track_visibility='onchange')
+    fechaDePago             = fields.Datetime(string = 'De pago',track_visibility='onchange')
+
+
+    #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    montoCubrirAdicional    = fields.Float(string = 'Monto a cubrir donde el receptor cubre adicional',track_visibility='onchange')
+
+    formaDeCobroAdicional   = fields.Selection((('Efectivo','Efectivo'), ('Descuento nómina','Descuento nómina')), string = "Forma de pago (Receptor)",track_visibility='onchange')
+
+    #Si es por efectivo
+    comoAplicaContablementeEfectivo    = fields.Selection((('Opcion','Opcion'),('Opcion','Opcion'),('Opcion','Opcion')), string = "Como aplica contablemente",track_visibility='onchange')
+
+    fechaLimiteDeReceptor   = fields.Datetime(string = 'Fecha límite devolución',track_visibility='onchange')
+
+    #Si es por descuento por nómina 
+    comoAplicaContablementeReceptorCubreAdicional = fields.Selection((('Opcion','Opcion'),('Opcion','Opcion'),('Opcion','Opcion')), string = "Como aplica contablemente",track_visibility='onchange')
 
 
 class motivos_gastos(models.Model):
