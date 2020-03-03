38802c4e5ac49b1c5ec7d4b9ee1a81e62c03fe18
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-03 15:07:03 -0600

    asasasasa

--- fac_order/models/models.py
+++ fac_order/models/models.py
@@ -14,9 +14,9 @@ class fac_order(models.Model):
       nameDos = fields.Char()
       month = fields.Selection([(1, 'January'), (2, 'February'), (3, 'March'), (4, 'April'),
                           (5, 'May'), (6, 'June'), (7, 'July'), (8, 'August'), 
-                          (9, 'September'), (10, 'October'), (11, 'November'), (12, 'December'), ], 
+                          (9, 'September'), (10, 'October'), (11, 'November'), (12, 'December')], 
                           string='Mes')
-      year = fields.Selection(get_years(), string='Año')
+      #year = fields.Selection(get_years(), string='Año')
             
             
       def get_years():
