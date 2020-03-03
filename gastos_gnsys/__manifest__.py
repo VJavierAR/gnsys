dd0122a89e4998ef147b69e29f58d6e7c35cb9a3
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-03 10:46:08 -0600

    Añadiendo modelo gastos

---
+++ gastos_gnsys/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    #Nombre de modelo
+    'name': "gastos_gnsys",
+
+    'summary': """
+        Gastos de GnSys""",
+
+    'description': """
+        Modelo de gastos de tipo GnSyS
+    """,
+
+    'author': "GNSYS",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+  'version': '12.0.1.0.0',
+    # any module necessary for this one to work correctly
+    #Modelo de dep
+    'depends': [
+        'mail',
+        'hr'
+    ],
+
+    # always loaded
+    #Algunos de modelo (Csi todas son vistas)
+    'data': [
+        #'security/ir.model.access.csv',
+        #'security/gastos_security.xml',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    #'demo': [
+     #   'demo/demo.xml',
+    #],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ gastos_gnsys/models/models.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, models, fields, api, tools
+from email.utils import formataddr
+from odoo.exceptions import UserError
+from odoo import exceptions, _
+import logging, ast
+import datetime, time
+_logger = logging.getLogger(__name__)
+
+class gastos_gnsys(models.Model):
+    _name = 'gastos'
+    _description = 'gastos_gnsys'
+
+    quienSolcita     = fields.One2many('hr.employee', 'gastoSolicitante', string = "Quien solita",track_visibility='onchange')
+    quienesAutorizan = fields.One2many('hr.employee' , string = "Quien (es) autorizan",track_visibility='onchange')
+    quienesReciben   = fields.One2many('hr.employee' , string = "Quien (es) reciben",track_visibility='onchange')
+
+    montoAprobado    = fields.float(string = 'Monto aprobado',track_visibility='onchange')
+    montoAtnticipado = fields.float(string = 'Monto anticipo',track_visibility='onchange')
+
+
+    formaDepago      = fields.selection((('Efectivo','Efectivo'), ('Cheque','Cheque'),('Depósito','Depósito'),('Transferencia','Transferencia')), string = "Forma de pago (Receptor)",track_visibility='onchange')
+
+
+    comoAplicaContablemente     = fields.selection((('Opcion','Opcion'),('Opcion','Opcion'),('Opcion','Opcion')), string = "Como aplica contablemente",track_visibility='onchange')
+    porCubrirAnticipo           = fields.datetime(string = 'Fecha límite de pago',track_visibility='onchange')
+
+    fechaPago                   = fields.datetime(string = 'Fecha de pago',track_visibility='onchange')
+
+    fechaLimiteDeComprobacion   = fields.datetime(string = 'Fecha límite de comprobación',track_visibility='onchange')
+
+
+    anticipoCubierto            = fields.float(string = 'Anticipo cubierto',track_visibility='onchange')
+
+    quienValida                 = fields.One2many('hr.employee', 'gastoValida', string = "Validado por",track_visibility='onchange')
+
+    motivos                     = fields.One2many('motivos', 'gasto', string = "Motivos",track_visibility='onchange')
+
+
+class motivos_gastos(models.Model):
+    _name = 'motivos'
+    _description = 'Motivos de un gasto'
+
+    gasto  = fields.Many2one('gatos', string = "Gasto ", track_visibility='onchange')
+    motivoDescripcion = fields.Text()
+    tipoDeMotivo      = fields.selection((('!','1'), ('2','2')), string = "Tipo de motivo",track_visibility='onchange')
+
+class empleados_gastos(models.Model):
+    _inherit = 'hr.employee'
+    
+    gastoSolicitante = fields.Many2one('gastos' string="Gasto solicitante")
+    gastoValida = fields.Many2one('gastos', string="Gasto valida")
+
+
+        
