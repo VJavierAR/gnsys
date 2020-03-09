ace77406811cc681db350c1d1b11b733570cab82
cesar <kliko1989@gmail.com>
2020-03-09 14:31:18 -0600

    dasdas

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -122,11 +122,11 @@ class StockCambio(TransientModel):
                 _logger.info("***************lista " + str(pp3))
 
                 #pp.ensure_one()
-                if(pp3['producto1']['id']!=pp3['producto1']['id']):
-                    self.env.cr.execute("delete from stock_move_line where reference='"+self.pick.name+"' and product_id="+pp.producto1.id+";")
-                    self.env.cr.execute("delete from stock_move where origin='"+self.pick.sale_id.name+"' and product_id="+pp.producto1.id+";")
-                    self.env.cr.execute("delete from sale_order_line where id="+str(s.id)+"' and product_id="+pp.producto1.id+";")
-                    datos={'order_id':self.pick.sale_id.id,'product_id':pp.producto2.id,'product_uom':pp.producto2.uom_id.id,'product_uom_qty':pp.cantidad,'name':pp.producto2.description if(pp.producto2.description) else '/','price_unit':0.00}
+                if(pp3[0]['producto1']['id']!=pp3[0]['producto1']['id']):
+                    self.env.cr.execute("delete from stock_move_line where reference='"+self.pick.name+"' and product_id="+pp3[0]['producto1']['id']+";")
+                    self.env.cr.execute("delete from stock_move where origin='"+self.pick.sale_id.name+"' and product_id="+pp3[0]['producto1']['id']+";")
+                    self.env.cr.execute("delete from sale_order_line where id="+str(s.id)+"' and product_id="+pp3[0]['producto1']['id']+";")
+                    datos={'order_id':self.pick.sale_id.id,'product_id':pp3[0]['producto2']['id'],'product_uom':pp3[0]['producto2']['uom_id']['id'],'product_uom_qty':pp3[0]['cantidad'],'name':pp3[0]['producto2']['description'],'price_unit':0.00}
                     ss=self.env['sale.order.line'].sudo().create(datos)
             self.env.cr.execute("update stock_picking set state='draft' where sale_id="+str(self.pick.sale_id.id)+";")
             self.env.cr.execute("select id from stock_picking where sale_id="+str(self.pick.sale_id.id)+";")
