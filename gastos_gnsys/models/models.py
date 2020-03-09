0c7622deb02cca30aa927102a8617ad1fe96c99a
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-09 11:33:31 -0600

    Update gastos

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -86,7 +86,7 @@ class gastos_gnsys(models.Model):
         #_logger.info()
         #estado = self.x_studio_field_VU6DU
         gasto=self.env['gastos'].search([('id', '=', self.id)])        
-        gasto.write({'x_studio_field_VU6DU' : 'aprobado','quienValida':self.env.user.id})
+        gasto.write({'x_studio_field_VU6DU' : 'aprobado', 'quienValida':self.env.user.name})
     
     
     
