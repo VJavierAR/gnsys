6ecdf49f83981a9d4ef0f5c331c73d8a8db02065
cesar <kliko1989@gmail.com>
2020-03-10 12:33:51 -0600

    ssss

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -85,7 +85,29 @@ class StockPickingMassAction(TransientModel):
                     if(l.picking_type_id.id==3):
                         l.sudo().write({'concentrado':CON})
                         self.env['stock.picking'].search([['sale_id','=',l.sale_id.id]]).write({'concentrado':CON})
-                return assigned_picking_lst.action_immediate_transfer_wizard().process()
+                pick_to_backorder = self.env['stock.picking']
+                pick_to_do = self.env['stock.picking']
+                for picking in assigned_picking_lst:
+                    # If still in draft => confirm and assign
+                    if picking.state == 'draft':
+                        picking.action_confirm()
+                        if picking.state != 'assigned':
+                            picking.action_assign()
+                            if picking.state != 'assigned':
+                                raise UserError(_("Could not reserve all requested products. Please use the \'Mark as Todo\' button to handle the reservation manually."))
+                    for move in picking.move_lines.filtered(lambda m: m.state not in ['done', 'cancel']):
+                        for move_line in move.move_line_ids:
+                            move_line.qty_done = move_line.product_uom_qty
+                    if picking._check_backorder():
+                        pick_to_backorder |= picking
+                        continue
+                    pick_to_do |= picking
+                # Process every picking that do not require a backorder, then return a single backorder wizard for every other ones.
+                if pick_to_do:
+                    pick_to_do.action_done()
+                if pick_to_backorder:
+                    return pick_to_backorder.action_generate_backorder_wizard()
+                        #return assigned_picking_lst.action_immediate_transfer_wizard().process()
 
             if assigned_picking_lst._check_backorder():
                 #assigned_picking_lst.write({'backorder':''})
