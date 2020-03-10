4bac01a294c275d5e34397e5b972e329b57c3849
cesar <kliko1989@gmail.com>
2020-03-10 03:10:09 -0600

    fvbghnj

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -113,7 +113,7 @@ class StockPickingMassAction(TransientModel):
                  #   'date_end': self.date_end,
                 #},
             }
-        return self.env.ref('stock_picking_mass_action.report_custom').report_action(self,data=data, docids=[], config=True)
+        return self.env.ref('stock_picking_mass_action.report_custom').report_action(self,data=data, config=True)
 
 
 class StockCambio(TransientModel):
