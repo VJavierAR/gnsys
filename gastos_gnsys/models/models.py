e1f426e0a254c4046d0fbe2f4641e021cfd14f4a
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-11 12:02:16 -0600

    update

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -177,14 +177,15 @@ class gastosEtapas(models.Model):
 class comprobaciones(models.Model):
     _name = 'gastos.comprobaciones'
     _description = 'Tipos de comprobaciónes del gasto'
-    comprobante  = fields.Many2one('gastos', string = "Comprobación ", track_visibility='onchange')
+    nombre                  = fields.Char(string="Nombre de comprobación", track_visibility='onchange')
+    comprobante             = fields.Many2one('gastos', string = "Comprobación ", track_visibility='onchange')
     concepto                = fields.Text(string = "Concepto",      track_visibility='onchange')
     descripcion             = fields.Text(string = "Descripción",   track_visibility='onchange')
     justificacon            = fields.Text(string = "Justificación", track_visibility='onchange')
     monto                   = fields.Float(string = "Monto",         track_visibility='onchange')
     comprobantes            = fields.Many2many('ir.attachment', string="Comprobantes")
     tipoDeComprobante       = fields.Selection((('Factura','Factura'),('FacturaSinIva','Factura sin IVA'),('TiketFacturable','Ticket facturable'),('Tiket','Ticket'),('Nota','Nota')), string = "Tipo de Comprobante",track_visibility='onchange')
-    porcentajeAceptado      = fields.Selection((('100','100%'),('75','75%'),('50','50%'),('25','25%'),('Nota','Nota')), string = "Porcentaje Aceptado",track_visibility='onchange')
+    porcentajeAceptado      = fields.Selection((('100','100%'),('75','75%'),('50','50%'),('25','25%'),('0%','0%')), string = "Porcentaje Aceptado",track_visibility='onchange')
     montoJustificado        = fields.Float(string = 'Monto justificado',track_visibility='onchange')
     cuentaContableDestino   = fields.Text(string = "Cuenta contable Destino",      track_visibility='onchange')
     centoDeCostos           = fields.Text(string = "Centro de Costos",      track_visibility='onchange')
