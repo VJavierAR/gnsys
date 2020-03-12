8a0827229d8dac9e544feb3d4f88ad369586a416
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-12 12:20:05 -0600

    Update

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -2076,7 +2076,7 @@ class helpdesk_update(models.Model):
             _logger.info("*****************self.x_studio_tecnico: " + str(self.x_studio_tecnico)
     """
 
-    diagnosticos = fields.One2many('helpdesk.diagnostico', 'ticket', string = 'Diagnostico')
+    #diagnosticos = fields.One2many('helpdesk.diagnostico', 'ticket', string = 'Diagnostico')
    
     order_line = fields.One2many('helpdesk.lines','ticket',string='Order Lines')
 class helpdesk_lines(models.Model):
@@ -2144,5 +2144,5 @@ class helpdes_diagnostico(object):
     _name = "helpdesk.diagnostico"
     _description = "Historial de diagnostico"
 
-    ticket = fields.Many2one('helpdesk.ticket', string = 'Ticket realcionado a diagnostico')
+    #ticket = fields.Many2one('helpdesk.ticket', string = 'Ticket realcionado a diagnostico')
     
