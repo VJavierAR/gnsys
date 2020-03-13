3f02b3afcb093a1417fb3ed91a0fb2bcb3e4c257
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-13 11:51:58 -0600

    saasas

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -34,15 +34,71 @@ class helpdesk_contadores(TransientModel):
     @api.depends('ticket_id')
     def _compute_contadorBNMesa(self):
         if self.ticket_id.x_studio_equipo_por_nmero_de_serie:
-            _logger.info("ticket: " + str(self.ticket_id) + 'series' + str(self.ticket_id.x_studio_equipo_por_nmero_de_serie) )
             for serie in self.ticket_id.x_studio_equipo_por_nmero_de_serie:
-
                 self.contadorBNMesa = int(serie.x_studio_contador_bn_mesa)
+                self.contadorColorMesa = int(serie.x_studio_contador_color_mesa)
     
     def modificarContadores(self):
-        self.env['helpdesk.diagnostico'].create({'ticketRelacion':self.ticket_id.id
-                                                ,'comentario':self.comentario
-                                                ,'estadoTicket':self.estado,'evidencia':[(6,0,self.evidencia.ids)]
-                                                ,'mostrarComentario':self.check
-                                                })
+        for record in self:  
+            for c in record.x_studio_equipo_por_nmero_de_serie:
+              if self.team_id.id==8:
+                q='helpdesk.ticket'
+              else:
+                q='stock.production.lot'
+              #if str(c.x_studio_field_A6PR9) =='Negro':
+              if str(c.x_studio_color_bn) == 'B/N':
+                  if int(c.x_studio_contador_bn_a_capturar) >= int(c.x_studio_contador_bn):
+                      if self.team_id.id==8:
+                         negrot = c.x_studio_contador_bn
+                         colort = c.x_studio_contador_color
+                      else:
+                         negrot = c.x_studio_contador_bn_mesa
+                         colort = c.x_studio_contador_color_mesa                        
+                      rr=self.env['dcas.dcas'].create({'serie' : c.id
+                                                    , 'contadorMono' : c.x_studio_contador_bn_a_capturar
+                                                    ,'x_studio_contador_color_anterior':colort
+                                                    , 'contadorColor' :c.x_studio_contador_color_a_capturar
+                                                    ,'x_studio_contador_mono_anterior_1':negrot
+                                                    ,'porcentajeNegro':c.x_studio__negro
+                                                    ,'porcentajeCian':c.x_studio__cian      
+                                                    ,'porcentajeAmarillo':c.x_studio__amarrillo      
+                                                    ,'porcentajeMagenta':c.x_studio__magenta
+                                                    ,'x_studio_descripcion':self.name
+                                                    ,'x_studio_tickett':self.x_studio_id_ticket
+                                                    ,'x_studio_hoja_de_estado':c.x_studio_evidencias
+                                                    ,'x_studio_usuariocaptura':self.env.user.name
+                                                    ,'fuente':q
+                                                    ,'x_studio_rendimiento':int(c.x_studio_rendimiento)/abs(int(c.x_studio_contador_bn_a_capturar)-int(negrot))
+                                                  })                  
+                      self.env['helpdesk.diagnostico'].create({'ticketRelacion':self.x_studio_id_ticket, 'estadoTicket': 'captura ', 'write_uid':  self.env.user.name, 'comentario':'capturas :' + str('Mono'+str(c.x_studio_contador_bn_a_capturar)+', Color '+str(c.x_studio_contador_color_a_capturar)+', Amarillo '+str(c.x_studio__amarrillo)+', Cian '+str(c.x_studio__cian)+', Negro '+str(c.x_studio__negro)+', Magenta '+str(c.x_studio__magenta)+', % de rendimiento '+str(rr.x_studio_rendimiento))})
+                  else :
+                    raise exceptions.ValidationError("Contador Monocromatico Menor")                     
+              #if str(c.x_studio_field_A6PR9) != 'Negro':       
+              if str(c.x_studio_color_bn) != 'B/N':
+                  if int(c.x_studio_contador_color_a_capturar) >= int(c.x_studio_contador_color) and int(c.x_studio_contador_bn_a_capturar) >= int(c.x_studio_contador_bn):                      
+                      if self.team_id.id==8:
+                         negrot = c.x_studio_contador_bn
+                         colort = c.x_studio_contador_color
+                      else:
+                         negrot = c.x_studio_contador_bn_mesa
+                         colort = c.x_studio_contador_color_mesa
+                      rr=self.env['dcas.dcas'].create({'serie' : c.id
+                                                    , 'contadorMono' : c.x_studio_contador_bn_a_capturar
+                                                    ,'x_studio_contador_color_anterior':colort
+                                                    , 'contadorColor' :c.x_studio_contador_color_a_capturar
+                                                    ,'x_studio_contador_mono_anterior_1':negrot
+                                                    ,'porcentajeNegro':c.x_studio__negro
+                                                    ,'porcentajeCian':c.x_studio__cian      
+                                                    ,'porcentajeAmarillo':c.x_studio__amarrillo      
+                                                    ,'porcentajeMagenta':c.x_studio__magenta
+                                                    ,'x_studio_descripcion':self.name
+                                                    ,'x_studio_tickett':self.x_studio_id_ticket
+                                                    ,'x_studio_hoja_de_estado':c.x_studio_evidencias
+                                                    ,'x_studio_usuariocaptura':self.env.user.name
+                                                    ,'fuente':q
+                                                    ,'x_studio_rendimiento':int(c.x_studio_rendimiento)/abs(int(c.x_studio_contador_bn_a_capturar)-int(negrot))
+                                                  })                  
+                      self.env['helpdesk.diagnostico'].create({'ticketRelacion':self.x_studio_id_ticket, 'estadoTicket': 'captura ', 'write_uid':  self.env.user.name, 'comentario':'capturas :' + str('Mono'+str(c.x_studio_contador_bn_a_capturar)+', Color '+str(c.x_studio_contador_color_a_capturar)+', Amarillo '+str(c.x_studio__amarrillo)+', Cian '+str(c.x_studio__cian)+', Negro '+str(c.x_studio__negro)+', Magenta '+str(c.x_studio__magenta)+', % de rendimiento '+str(rr.x_studio_rendimiento))})
+                  else :
+                    raise exceptions.ValidationError("Error al capturar debe ser mayor")
 
