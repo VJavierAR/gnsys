988015eddad044c15138949dd84d55bcc64ce4fd
cesar <kliko1989@gmail.com>
2020-03-13 05:08:38 -0600

    dsadsad

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -107,25 +107,26 @@ class StockPickingMassAction(TransientModel):
             # Process every picking that do not require a backorder, then return a single backorder wizard for every other ones.
             if pick_to_do:
                 pick_to_do.action_done()
-            if pick_to_backorder:
-                _logger.info("***************lista2" + str(len(pick_to_backorder.name)))
+            #if pick_to_backorder:
+            #    _logger.info("***************lista2" + str(len(pick_to_backorder.name)))
                 #wiz = self.env['stock.backorder.confirmation'].create({'pick_ids': [(4, pick_to_backorder.id)]})
                 #wiz.process()
                 #pick_to_backorder.action_done()
+            if assigned_picking_lst._check_backorder():
                 cancel_backorder=True
-                # if cancel_backorder:
-                #    for pick_id in self.pick_ids:
-                #        moves_to_log = {}
-                #        for move in pick_id.move_lines:
-                #            if float_compare(move.product_uom_qty, move.quantity_done, precision_rounding=move.product_uom.rounding) > 0:
-                #                moves_to_log[move] = (move.quantity_done, move.product_uom_qty)
-                #        pick_id._log_less_quantities_than_expected(moves_to_log)
-                # self.pick_ids.action_done()
-                # if cancel_backorder:
-                #    for pick_id in self.pick_ids:
-                #        backorder_pick = self.env['stock.picking'].search([('backorder_id', '=', pick_id.id)])
-                #        backorder_pick.action_cancel()
-                #        pick_id.message_post(body=_("Back order <em>%s</em> <b>cancelled</b>.") % (",".join([b.name or '' for b in backorder_pick])))
+                if cancel_backorder:
+                   for pick_id in self.pick_ids:
+                       moves_to_log = {}
+                       for move in pick_id.move_lines:
+                           if float_compare(move.product_uom_qty, move.quantity_done, precision_rounding=move.product_uom.rounding) > 0:
+                               moves_to_log[move] = (move.quantity_done, move.product_uom_qty)
+                       pick_id._log_less_quantities_than_expected(moves_to_log)
+                self.pick_ids.action_done()
+                if cancel_backorder:
+                   for pick_id in self.pick_ids:
+                       backorder_pick = self.env['stock.picking'].search([('backorder_id', '=', pick_id.id)])
+                       backorder_pick.action_cancel()
+                       pick_id.message_post(body=_("Back order <em>%s</em> <b>cancelled</b>.") % (",".join([b.name or '' for b in backorder_pick])))
                 #             #return pick_to_backorder.action_generate_backorder_wizard()
                         #return assigned_picking_lst.action_immediate_transfer_wizard().process()
 
