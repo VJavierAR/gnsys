2dc4aff1a71e8e1803a9757d1d0187e203bf48ed
cesar <kliko1989@gmail.com>
2020-03-13 02:58:02 -0600

    ssss

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -27,10 +27,10 @@ class StockPicking(Model):
 
     @api.depends('state')
     def back(self):
-        self.ensure_one()
-        if(self.state=="assigned"):
+        for r in self:
+        if(r.state=="assigned"):
             if(self._check_backorder()):
-                self.mensaje="Al confirmar se generara un backorder"
+                r.mensaje="Al confirmar se generara un backorder"
                 
     @api.onchange('carrier_tracking_ref')
     def agregarNumeroGuiaATicketOSolicitud(self):
