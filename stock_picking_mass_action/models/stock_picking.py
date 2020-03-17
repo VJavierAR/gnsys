d75580a3110a7ebd00504f01fd7129d9eb169efc
cesar <kliko1989@gmail.com>
2020-03-17 14:08:54 -0600

    sasasaa

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -23,7 +23,7 @@ class StockPicking(Model):
     concentrado=fields.Char()
     mensaje=fields.Char(compute='back')
     sale_child=fields.Many2one('sale.order')
-    back=fields.Boolean(related='sale_id.x_studio_backorder',store=True)
+    back=fields.Boolean(related='sale_id.x_studio_backorder')
     #documentosDistro = fields.Many2many('ir.attachment', string="Evidencias ")
     #historialTicket = fields.One2many('ir.attachment','res_id',string='Evidencias al ticket',store=True,track_visibility='onchange')
 
