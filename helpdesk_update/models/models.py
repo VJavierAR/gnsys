d774f6e9f94563eb506bee2c1842c3d65359ba1a
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-18 10:48:32 -0600

    sasasas

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -24,7 +24,7 @@ class helpdesk_update(models.Model):
     x_studio_empresas_relacionadas = fields.Many2one('res.partner', store=True, track_visibility='onchange', string='Localidad')
     historialCuatro = fields.One2many('x_historial_helpdesk','x_id_ticket',string='historial de ticket estados',store=True,track_visibility='onchange')
     documentosTecnico = fields.Many2many('ir.attachment', string="Evidencias")
-    ultimoEvidencia = fields.Many2many('ir.attachment', string="Ultima evidencia")
+    ultimoEvidencia = fields.Many2many('ir.attachment', string="Ultima evidencia",readonly=True)
     stage_id = fields.Many2one('helpdesk.stage', string='Stage', ondelete='restrict', track_visibility='onchange',group_expand='_read_group_stage_ids',readonly=True,copy=False,index=True, domain="[('team_ids', '=', team_id)]")
     productos = fields.One2many('product.product','id',string='Solicitudes',store=True)
     
