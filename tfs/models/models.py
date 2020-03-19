394d588da892529871c60f6f39d488030a0d942a
cesar <kliko1989@gmail.com>
2020-03-19 14:04:56 -0600

    asda

--- tfs/models/models.py
+++ tfs/models/models.py
@@ -150,6 +150,7 @@ class tfs(models.Model):
     def type(self):
         for record in self:
             dc=self.env['dcas.dcas'].search([['serie','=',record.serie.id],['fuente','=','tfs.tfs'],['x_studio_toner_'+str(record.tipo).lower(),'=',1]]).sorted(key='create_date',reverse=True)
+            _logger.info(str(dc))
             if(len(dc)>0):
                 record['contadorAnterior']=dc[0].id 
 
