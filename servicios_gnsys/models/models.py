581ed025db91513639c05d116a19b21ca3f3131f
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-03-19 15:07:47 -0600

    erver

--- servicios_gnsys/models/models.py
+++ servicios_gnsys/models/models.py
@@ -150,7 +150,7 @@ class contratos(models.Model):
     @api.onchange('cliente')
     def cambiarRazonSocial(self):
         if self.cliente :
-            self.razonSocial = self.cliente.razonSocial
+            self.razonSocial = self.cliente.razonSocial.name
             self.direccion = self.cliente.contact_address
             self.ejecutivoDeCuenta = self.cliente.x_studio_ejecutivo
             self.vendedor = self.cliente.x_studio_vendedor
