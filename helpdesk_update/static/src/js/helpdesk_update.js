9fa0c0c9e3aa0e1c5b6b047a21bf1845552f7873
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-21 14:21:03 -0600

    Up

--- helpdesk_update/static/src/js/helpdesk_update.js
+++ helpdesk_update/static/src/js/helpdesk_update.js
@@ -1,4 +1,5 @@
 
+/*
 function test() {
     console.log("Hola mundo");
     
@@ -8,3 +9,39 @@ function test() {
     //document.getElementsByClassName("o_field_char o_field_widget o_required_modifier field_name").style.color = "red";    
     
 }
+*/
+
+
+
+odoo.define('helpdesk_update.tree_view_button', function (require){
+"use strict";
+
+	var core = require('web.core');
+	var ListView = require('web.ListView');
+	var QWeb = core.qweb;
+
+	ListView.include({
+
+        render_buttons: function($node) {
+            var self = this;
+            this._super($node);
+                this.$buttons.find('.o_list_tender_button_create').click(this.proxy('tree_view_action'));
+        },
+
+        tree_view_action: function () {
+
+	        this.do_action({
+	                type: "ir.actions.act_window",
+	                name: "Series",
+	                res_model: "helpdesk.ticket",
+	                views: [[false,'form']],
+	                target: 'current',
+	                view_type : 'form',
+	                view_mode : 'form',
+	                flags: {'form': {'action_buttons': true, 'options': {'mode': 'edit'}}}
+	        });
+	        return { 'type': 'ir.actions.client'
+	        		,'tag': 'reload', } 
+	    }
+	});
+});
