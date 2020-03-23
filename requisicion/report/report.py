72f94413975d51156954c31dd35545afcfbb80fc
Cesar <kliko1989@hotmail.com>
2020-03-23 17:37:03 -0600

    dfghj

--- requisicion/report/report.py
+++ requisicion/report/report.py
@@ -1,18 +1,13 @@
 from odoo import models
-import logging, ast
-_logger = logging.getLogger(__name__)
-
-
 
 class PartnerXlsx(models.AbstractModel):
-    _name = 'report.module_name.report_name'
+    _name = 'report.requisicion.partner_xlsx'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, partners):
-        #for obj in partners:
-        #    report_name = obj.name
+        for obj in partners:
+            report_name = obj.name
             # One sheet by partner
-        sheet = workbook.add_worksheet("clientes")
-        #    bold = workbook.add_format({'bold': True})
-        #    sheet.write(0, 0, obj.name, bold)
-        _logger.info("HOLAAAAAAAAAAAAAAAAA")
+            sheet = workbook.add_worksheet(report_name[:31])
+            bold = workbook.add_format({'bold': True})
+            sheet.write(0, 0, obj.name, bold)
