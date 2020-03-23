fc21dcb60925580462b49b6a2a330faab7ddd756
Cesar <kliko1989@hotmail.com>
2020-03-23 17:13:45 -0600

    el Gerardo y su datetime xd

--- requisicion/report/report.py
+++ requisicion/report/report.py
@@ -9,10 +9,10 @@ class PartnerXlsx(models.AbstractModel):
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, partners):
-        for obj in partners:
-            report_name = obj.name
+        #for obj in partners:
+        #    report_name = obj.name
             # One sheet by partner
-            sheet = workbook.add_worksheet(report_name[:31])
-            bold = workbook.add_format({'bold': True})
-            sheet.write(0, 0, obj.name, bold)
-            _logger.info(str(obj.name))
+        sheet = workbook.add_worksheet("clientes")
+        #    bold = workbook.add_format({'bold': True})
+        #    sheet.write(0, 0, obj.name, bold)
+        _logger.info("HOLAAAAAAAAAAAAAAAAA")
