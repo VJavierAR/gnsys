793a12337ffcb4a9f30440a3603e5f63dc041bea
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-23 00:47:30 -0600

    Up

--- helpdesk_update/static/src/js/helpdesk_update.js
+++ helpdesk_update/static/src/js/helpdesk_update.js
@@ -17,6 +17,50 @@ odoo.define('helpdesk_update.tree_view_button', function (require){
 "use strict";
 
 	var core = require('web.core');
+	var ListController = require('web.ListController');
+	var rpc = require('web.rpc');
+	var session = require('web.session');
+	var _t = core._t;
+
+	ListController.include({
+		renderButtons: function($node) {
+		this._super.apply(this, arguments);
+		   if (this.$buttons) {
+		     this.$buttons.find('.oe_action_button').click(this.proxy('action_def'));
+		   }
+		},
+
+		action_def: function () {
+            var self = this
+            var user = session.uid;
+            rpc.query({
+                model: 'helpdesk.ticket',
+                method: 'cambio_wizard',
+                args: [[user],{'id':user}],
+            });
+        },
+
+        receive_invoice: function () {
+            var self = this
+            var user = session.uid;
+            rpc.query({
+                model: 'helpdesk.ticket',
+                method: 'cambio_wizard',
+                args: [[user],{'id':user}],
+                }).then(function (e) {
+                    self.do_action({
+                        name: _t('action_invoices'),
+                        type: 'ir.actions.act_window',
+                        res_model: 'name.name',
+                        views: [[false, 'form']],
+                        view_mode: 'form',
+                        target: 'new',
+                    });
+                    window.location
+                });
+            },
+	});
+	/*
 	var ListView = require('web.ListView');
 	var QWeb = core.qweb;
 
@@ -44,4 +88,5 @@ odoo.define('helpdesk_update.tree_view_button', function (require){
 	        		,'tag': 'reload', } 
 	    }
 	});
+	*/
 });
