c9547abaa3dcadb1c326c3a7b3377663c5446a48
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-25 14:05:06 -0600

    a

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -51,6 +51,18 @@ class helpdesk_update(models.Model):
             self.env.cr.execute(query)
             self.env.cr.commit()
 
+    @api.depends('x_studio_empresas_relacionadas')
+    def cambiaContactoLocalidad(self):
+        _logger.info("Entre por toner")
+        if self.x_studio_empresas_relacionadas:
+            _logger.info("Entre por toner: " + str(self.x_studio_empresas_relacionadas))
+            loc = self.x_studio_empresas_relacionadas.id
+            idLoc = self.env['res.partner'].search([['parent_id', '=', loc],['x_studio_subtipo', '=', 'Contacto de localidad']], order='create_date desc', limit=1).id
+            self.localidadContacto = idLoc
+            query = "update helpdesk_ticket set \"localidadContacto\" = " + str(idLoc) + ", \"x_studio_field_6furK\" = " + str(self.x_studio_empresas_relacionadas.x_studio_field_SqU5B) + " where id = " + str(self.x_studio_id_ticket) + ";"
+            self.env.cr.execute(query)
+            self.env.cr.commit()
+
     @api.model
     def _contacto_definido(self):
         if self.x_studio_empresas_relacionadas:
