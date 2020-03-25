ce5b9c6f9a9ad1f95aa93d28aaa6ee7fd89e2597
cesar <kliko1989@gmail.com>
2020-03-25 02:16:26 -0600

    dfvgbhnjmk

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -254,13 +254,13 @@ class TransferInter(TransientModel):
 
     def confirmar(self):
         sale = self.env['sale.order'].create({'partner_id' : 1, 'origin' : 'Transferencia Interna' , 'warehouse_id' : self.almacenOrigen.id, 'team_id' : 1})
-        for l in self.lines:
-            datosr={'order_id' : sale.id, 'product_id' : l.producto.id, 'product_uom_qty' : l.cantidad}
-            h=self.env['stock.location.route'].search([['name','=',str(str(self.almacenDestino.name)+": proveer producto de "+str(self.almacenOrigen.name))]])
-            if(h):
-                datosr['route_id']=h[0].id
-            self.env['sale.order.line'].create(datosr)
-        sale.action_confirm()
+        # for l in self.lines:
+        #     datosr={'order_id' : sale.id, 'product_id' : l.producto.id, 'product_uom_qty' : l.cantidad}
+        #     h=self.env['stock.location.route'].search([['name','=',str(str(self.almacenDestino.name)+": proveer producto de "+str(self.almacenOrigen.name))]])
+        #     if(h):
+        #         datosr['route_id']=h[0].id
+        #     self.env['sale.order.line'].create(datosr)
+        # sale.action_confirm()
 
 
 
