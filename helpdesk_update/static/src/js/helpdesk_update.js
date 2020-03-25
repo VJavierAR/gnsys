47ea0b5b33a77be4845e3b4af01698bf8482a933
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-24 20:26:18 -0600

    a

--- helpdesk_update/static/src/js/helpdesk_update.js
+++ helpdesk_update/static/src/js/helpdesk_update.js
@@ -26,7 +26,7 @@ odoo.define('invoice.action_button', function (require) {
 		this._super.apply(this, arguments);
 			if (this.$buttons) {
 		    	this.$buttons.find('.oe_action_button').click(this.proxy('action_def'));
-		    	this,$buttons.find('.o_list_button_add').hide()
+		    	this,$buttons.find('.o_list_button_add').hide();
 		   	}
 		},
 
