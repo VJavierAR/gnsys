5f8a232f5eb794bc4bbfc23b87513f0e74638fec
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-26 03:27:10 -0600

    asasasas

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -91,6 +91,8 @@ class dcas(models.Model):
         
         _logger.info("c inicio"+str(c.tablahtml))
         _logger.info("self inicio id"+str(c.id))
+        _logger.info("self inicio id"+str(c.create_date))
+        self.env.cr.execute("update dcas_dcas set x_studio_fecha = '"+c.create_date+"' where  id = " + str(c.id) + ";")
         """
         contaC=c.contadorColor                       
         cac=c.contadorAnteriorColor
