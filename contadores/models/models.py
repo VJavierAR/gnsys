53b8b74acde0b7ba7af989f0cbb0cfbd666c7f2d
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-26 00:31:46 -0600

    asas

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -128,7 +128,7 @@ class dcas(models.Model):
            self.env.cr.execute("update dcas_dcas set x_studio_ultima_ubicacin = '"+str(c.x_studio_ultima_ubicacin)+"' where  id = " + str(c.id) + ";")
            _logger.info("c color"+str(c.x_studio_color_o_bn)) 
            _logger.info("c negro anterior"+str(c.contadorAnteriorNegro))
-            logger.info("self negro anterior"+str(self.contadorAnteriorNegro))
+           _logger.info("self negro anterior"+str(self.contadorAnteriorNegro))
            self.env.cr.execute("update dcas_dcas set x_studio_color_o_bn = '"+str(c.x_studio_color_o_bn)+"' where  id = " + str(c.id) + ";")           
            self.env.cr.execute("update dcas_dcas set tablahtml = '"+c.tablahtml+"' where  id = " + str(c.id) + ";")
            _logger.info("c final"+str(c.tablahtml))
