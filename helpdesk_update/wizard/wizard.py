eff70c0f2c8bcec32d66c60d84aee9648397ef48
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-03-26 00:53:07 -0600

    Cerrado por doc de tecnico

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -29,6 +29,8 @@ class HelpDeskComentario(TransientModel):
             self.ticket_id.write({'x_studio_zona': self.zona
                                 , 'x_studio_field_6furK': self.zona
                                 })
+        if ticket_id.env.user.in_group_172:
+            self.ticket_id.write({'stage_id': 18})
         mess = 'Diagnostico / Comentario añadido al ticket "' + str(self.ticket_id.id) + '" de forma exitosa. \n\nComentario agregado: ' + str(self.comentario) + '. \n\nGenerado en el estado: ' + self.ticket_id.stage_id.name
         wiz = self.env['helpdesk.alerta'].create({'ticket_id': self.ticket_id.id, 'mensaje': mess})
         view = self.env.ref('helpdesk_update.view_helpdesk_alerta')
