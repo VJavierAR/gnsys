2936fa8ab3d48a85cfcb69a6ed79da32b55e4f6a
cesar <kliko1989@gmail.com>
2020-03-27 07:40:19 -0600

    ssss

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -162,7 +162,7 @@ class StockCambio(TransientModel):
             dt=[]
             al=[]
             for sa in self.pick.move_ids_without_package:
-                d=list(filter(lambda x:x['producto1']==sa.product_id.id,pro_ids))
+                d=list(filter(lambda x:x['producto1']==sa.product_id.id,self.pro_ids))
                 if(sa.product_id.id!=d[0]['producto2']['id']):
                     self.env.cr.execute("delete from stock_move_line where reference='"+self.pick.name+"' and product_id="+str(sa.product_id.id)+";")
                     self.env.cr.execute("delete from stock_move where origin='"+self.pick.sale_id.name+"' and product_id="+str(sa.product_id.id)+";")
