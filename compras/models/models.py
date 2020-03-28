ec84e6528fa9877b0c4946555d84e89d6c2e1ed8
cesar <kliko1989@gmail.com>
2020-03-28 01:31:46 -0600

    dsdss

--- compras/models/models.py
+++ compras/models/models.py
@@ -174,9 +174,7 @@ class compras(models.Model):
                                 product['price_unit']=float(row[12].value)-(float(row[12].value)*.02)
                                 product['taxes_id']=[10]
                             if("CTR" in row[0].value):
-                                descuento=float(row[16].value)*.16 if(row[16].ctype!=0) else 0
-                                if(cantidad>0):
-                                    product['price_unit']=(float(row[16].value)-(descuento))/cantidad
+                                #product['price_subtotal']=float(row[12].value)-(float(row[12].value)*.02)
                                 product['taxes_id']=[10]
                             arr.append(product)
                     if(len(arr)>0):
