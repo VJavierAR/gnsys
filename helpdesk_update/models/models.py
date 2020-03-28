c748003a87ec36e71ad84eb6a1165060d3bc3f2d
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-03-28 03:38:22 -0600

    sadasd

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -2138,11 +2138,11 @@ class helpdesk_update(models.Model):
                 for localidades in self.x_studio_equipo_por_nmero_de_serie_1:
                     if self.x_studio_equipo_por_nmero_de_serie_1[0].ultimaUbicacion != localidades.ultimaUbicacion:
                        raise exceptions.ValidationError("Error "+str(self.x_studio_equipo_por_nmero_de_serie_1[0].ultimaUbicacion)+' deben ser la misma localidad '+localidades.ultimaUbicacion)
-                raise exceptions.ValidationError("tamaño "+str(len(self.x_studio_equipo_por_nmero_de_serie_1))+' ids '+ str(self.x_studio_equipo_por_nmero_de_serie_1.ids)+' serie '+str(self.x_studio_equipo_por_nmero_de_serie_1[len(self.x_studio_equipo_por_nmero_de_serie_1)-1].id))
+                raise exceptions.ValidationError("tamaño "+str(len(self.x_studio_equipo_por_nmero_de_serie_1))+' ids '+ str(self.x_studio_equipo_por_nmero_de_serie_1.ids)+' serie '+str(self.x_studio_equipo_por_nmero_de_serie_1[len(self.x_studio_equipo_por_nmero_de_serie_1)-1].serie.name))
                 for serie in self.x_studio_equipo_por_nmero_de_serie_1:
                     for serieDuplicada in self.x_studio_equipo_por_nmero_de_serie_1:                    
                         if serie.id == serieDuplicada.id:
-                            raise exceptions.ValidationError("Error serie ya agregada"+str(serieDuplicada.name)+' '+str(serie.name))
+                            raise exceptions.ValidationError("Error serie ya agregada"+str(serieDuplicada.serie.name)+' '+str(serie.serie.name))
                 
             
             queryt="select h.id from helpdesk_ticket_stock_production_lot_rel s, helpdesk_ticket h where h.id=s.helpdesk_ticket_id and h.id!="+str(self.x_studio_id_ticket)+"  and h.stage_id!=18 and h.team_id=8 and  h.active='t' and stock_production_lot_id = "+str(self.x_studio_equipo_por_nmero_de_serie_1[0].serie.id)+" limit 1;"            
