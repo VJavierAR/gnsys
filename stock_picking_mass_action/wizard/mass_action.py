07a05719df5b1cf7d89fe1bf6befa422a2c5663e
cesar <kliko1989@gmail.com>
2020-03-28 03:07:16 -0600

    ssssssssss

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -52,12 +52,11 @@ class StockPickingMassAction(TransientModel):
 
     @api.depends('picking_ids')
     def che(self):
-        for s in picking_ids:
+        for s in self.picking_ids:
             if(s.picking_type_id.id==3):
                 check=2
             if(s.picking_type_id.id==29314):
                 check=1
-
     @api.multi
     def mass_action(self):
         self.ensure_one()
@@ -91,10 +90,11 @@ class StockPickingMassAction(TransientModel):
             CON=str(self.env['ir.sequence'].next_by_code('concentrado'))
             for l in assigned_picking_lst:
                 if(l.picking_type_id.id==3):
+                    self.check=2
                     #l.sudo().write({'concentrado':CON})
                     self.env['stock.picking'].search([['sale_id','=',l.sale_id.id]]).write({'concentrado':CON})
                 if(l.picking_type_id.id==29314):
-                    
+                    self.check=1
             pick_to_backorder = self.env['stock.picking']
             pick_to_do = self.env['stock.picking']
             for picking in assigned_picking_lst:
