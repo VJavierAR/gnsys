94340acacaf46ea8654722d50520c99b9617d246
cesar <kliko1989@gmail.com>
2020-03-28 00:13:08 -0600

    sssssss

--- compras/models/models.py
+++ compras/models/models.py
@@ -161,6 +161,7 @@ class compras(models.Model):
 
 
                         if(row[0].value in self.partner_id.name and str(row[0].ctype)!='0'):
+                            product={}
                             producto=row[2].value
                             precio=float(row[12].value)
                             cantidad=int(row[10].value)
