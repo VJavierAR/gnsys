35bd12392e1330b909c752ab6e147b146907d5df
Cesar <kliko1989@hotmail.com>
2020-03-30 16:44:44 -0600

    Update report.py
    ded

--- stock_picking_mass_action/report/report.py
+++ stock_picking_mass_action/report/report.py
@@ -11,9 +11,9 @@ class PartnerXlsx(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, partners):
         i=1
         d=[]
-        report_name = 'Clientes'
+        report_name = 'Movimientos'
         bold = workbook.add_format({'bold': True})
-        sheet = workbook.add_worksheet('Clientes')
+        sheet = workbook.add_worksheet('Movimientos')
         #sheet.set_header('A1', {'image_left': 'logo.png'})
         #sheet.write(0, 0, 'Cliente', bold)
         #sheet.insert_image('A1', 'logo.png')
@@ -25,8 +25,8 @@ class PartnerXlsx(models.AbstractModel):
             # One sheet by partner
             
             #_logger.info('work')
-            sheet.write(i, 0, obj.name, bold)
-            sheet.write(i, 1, obj.street, bold)
+            sheet.write(i, 0, obj.reference, bold)
+            sheet.write(i, 1, obj.date, bold)
             i=i+1
         sheet.add_table('A1:B'+str((i+1)),{'columns': [{'header': 'Cliente'},{'header': 'Calle'}]})
         workbook.close()
