c72371074a9c530e3db51944ed2563faff0d17cd
Cesar <kliko1989@hotmail.com>
2020-04-01 14:11:53 -0600

    dsaaaaaaaaaa

--- stock_picking_mass_action/report/report.py
+++ stock_picking_mass_action/report/report.py
@@ -33,15 +33,27 @@ class PartnerXlsx(models.AbstractModel):
             #_logger.info('work')
             sheet.write(i, 0, obj.x_studio_field_aVMhn.name, bold)
             sheet.write(i, 1, obj.date.strftime("%Y/%m/%d"), bold)
-            sheet.write(i, 2, obj.x_studio_field_3lDS0.name, bold)
+            if(obj.x_studio_ticket):
+                sheet.write(i, 2, obj.x_studio_ticket, bold)
+            if(obj.x_studio_ticket==False):
+                sheet.write(i, 2, obj.x_studio_ticket, bold)
+            sheet.write(i, 3, obj.x_studio_field_3lDS0.name, bold)
             if(obj.location_id.id==obj.x_studio_field_3lDS0.wh_input_stock_loc_id.id):
-                sheet.write(i, 3, "Entrada", bold)
-            else:
-                sheet.write(i, 3, "Salida", bold)
-            sheet.write(i, 4, obj.product_id.name, bold)
-            sheet.write(i, 5, obj.product_id.default_code, bold)
-            sheet.write(i, 6, obj.qty_done, bold)
-            sheet.write(i, 7, obj.move_id.picking_id.partner_id.name, bold)
+                sheet.write(i, 4, "Entrada", bold)
+            if(obj.location_id.id!=obj.x_studio_field_3lDS0.wh_input_stock_loc_id.id):
+                sheet.write(i, 4, "Salida", bold)
+
+            sheet.write(i, 5, obj.product_id.name, bold)
+            sheet.write(i, 6, obj.product_id.default_code, bold)
+            if(obj.lot_id):
+                sheet.write(i, 7, obj.lot_id.name, bold)
+            if(obj.lot_id==False):
+                sheet.write(i, 7, obj.move_id.x_studio_serieorderline, bold)
+
+            sheet.write(i, 8, obj.qty_done, bold)
+            sheet.write(i, 9, obj.move_id.picking_id.partner_id.parent_id.name, bold)
+            sheet.write(i, 10, obj.move_id.picking_id.partner_id.name, bold)
+
             i=i+1
-        sheet.add_table('A2:H'+str((i)),{'style': 'Table Style Medium 9','columns': [{'header': 'Categoria'},{'header': 'Fecha'},{'header':'Almacen'},{'header': 'Tipo'},{'header': 'Modelo'},{'header': 'No Parte'},{'header': 'Cantidad'},{'header': 'Cliente'}]})
+        sheet.add_table('A2:H'+str((i)),{'style': 'Table Style Medium 9','columns': [{'header': 'Categoria'},{'header': 'Fecha'},{'header': 'Documento Origen'},{'header':'Almacen'},{'header': 'Tipo'},{'header': 'Modelo'},{'header': 'No Parte'},{'header': 'Serie'},{'header': 'Cantidad'},{'header': 'Cliente'},{'header': 'Localidad'}]})
         workbook.close()
