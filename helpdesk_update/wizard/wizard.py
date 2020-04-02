d718359163e211e5318d514c6d430d51c7db5732
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-04-01 22:07:17 -0600

    me falto unos igual jajjaa

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -472,8 +472,8 @@ class helpdesk_crearconserie(TransientModel):
     _description = 'HelpDesk crear ticket desde la serie'
 
     serie = fields.Many2many('stock.production.lot', string = 'Serie')
-    clienteRelacion fields.Many2one('res.partner' string = 'Cliente')
-    localidadRelacion fields.Many2one('res.partner' string = 'Localidad')
+    clienteRelacion = fields.Many2one('res.partner' string = 'Cliente')
+    localidadRelacion = fields.Many2one('res.partner' string = 'Localidad')
 
     cliente = fields.Text(string = 'Cliente')
     idCliente = fields.Text(string = 'idCliente', store=True)
@@ -549,7 +549,7 @@ class helpdesk_crearconserie(TransientModel):
         self.telefonoContactoLocalidad = self.localidadRelacion.phone
         self.movilContactoLocalidad = self.localidadRelacion.mobile
         self.correoContactoLocalidad = self.localidadRelacion.email
-        
+
         self.direccionCalleNombre = self.localidadRelacion.street_name
         self.direccionNumeroExterior = self.localidadRelacion.street_number
         self.direccionNumeroInterior = self.localidadRelacion.street_number2
