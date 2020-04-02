065760c3424b5489a54cb950549840e223a3af5d
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-04-02 02:26:59 -0600

    prueba

--- servicios_gnsys/models/models.py
+++ servicios_gnsys/models/models.py
@@ -190,15 +190,28 @@ class contratos(models.Model):
             self.direccion = self.cliente.contact_address
             self.ejecutivoDeCuenta = self.cliente.x_studio_ejecutivo
             self.vendedor = self.cliente.x_studio_vendedor
+    @api.onchange('fechaDeFinDeContrato')
     @api.multi
     def expiracionServicios(self):
-        for record in self:
-            _logger.info("-------Logger de OSWALDO "+str(record.mapped('servicio.servActivo')))
-            for elemento in record.mapped('servicio'):
-                _logger.info("-------Logger de OSWALDO*****"+str(elemento.servActivo))
-                if elemento:
-                    elemento.servActivo = False
-
+        if self.cliente:
+            for record in self:
+                #_logger.info("-------Logger de OSWALDO "+str(record.mapped('servicio.servActivo')))
+                for elemento in record.mapped('servicio'):
+                    #_logger.info("-------Logger de OSWALDO*****"+str(elemento.servActivo))
+                    if elemento:
+                        elemento.servActivo = False
+    #    for record in self:       
+    #         if record.contrato:
+    #             _logger.info("-------Logger de OSWALDO "+str(record.contrato.name))
+    #             fecha = str(record.fechaDeFinDeContrato).split(' ')[0]
+    #             converted_date = datetime.datetime.strptime(fecha, '%Y-%m-%d').date()
+    #             fechaCompara = (datetime.date.today() - converted_date).days
+    #             #Comparamos la fecha de hoy con la fecha de fin de contrato
+    #             #Aqui obtenemos todos los serviciós
+    #             if fechaCompara > 0:
+    #                 if record.servicio :
+    #                     for servicio in record.servicio: 
+    #                         servicio.servActivo = False
 
 class cliente_contratos(models.Model):
     _inherit = 'res.partner'
