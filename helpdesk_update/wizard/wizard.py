b9b52b27ffd1d4a3abfd0d4c8f34d00bb99cb1f6
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-04-03 18:01:42 -0600

    aaa

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -495,7 +495,7 @@ class helpdesk_crearconserie(TransientModel):
     direccionCodigoPostal = fields.Text(string = 'Código postal')
 
     ticket_id_existente = fields.Integer(string = 'Ticket existente', default = 0)
-    textoTicketExistente = fields.Text(string = '')
+    textoTicketExistente = fields.Text(string = ' ')
 
     def abrirTicket(self):
         return {
@@ -694,7 +694,7 @@ class helpdesk_crearconserie(TransientModel):
                     wiz = ''
                     mensajeTitulo = "Ticket generado!!!"
                     if len(informacion) > 0:
-                      self.textoTicketExistente = "<h1Esta serie ya tiene un ticket en proceso.</h1><br/><br/><h3>El ticket en proceso es:" + str(informacion[0][0]) + "</h3>"
+                      self.textoTicketExistente = "<br/><br/><h1>Esta serie ya tiene un ticket en proceso.</h1><br/><br/><h3>El ticket en proceso es:" + str(informacion[0][0]) + "</h3>"
                       self.ticket_id_existente = informacion[0][0]
                       mensajeTitulo = "Alerta!!!"
                       mensajeCuerpo = "Esta serie ya tiene un ticket en proceso.\n\nEl ticket en proceso es:" + str(informacion[0][0])
