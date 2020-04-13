a81ed74d4a847fbd792706538e2f60cf56f58339
Cesar <kliko1989@hotmail.com>
2020-04-13 17:32:24 -0500

    dsadas

--- stock_picking_mass_action/static/src/js/stock_pick.js
+++ stock_picking_mass_action/static/src/js/stock_pick.js
@@ -21,7 +21,8 @@ odoo.define('invoice.action_button', function (require) {
                     if (this.actionViews[0].viewID == 2941) {
                         this.$buttons.find('.o_button_import').hide();
                         this.$buttons.find('.o_list_button_add').hide();
-                        this.$buttons.find('.oe_action_button').click(this.proxy('action_inter2'));                    }
+                        this.$buttons.find('.oe_action_button').click(this.proxy('action_inter2'));
+                    }
                     else{
                         this.$buttons.find('.oe_action_button_ticket_report').hide();
                         this.$buttons.find('.oe_action_button').hide();
