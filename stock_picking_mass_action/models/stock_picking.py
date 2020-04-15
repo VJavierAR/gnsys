edf6cf26d52da8da478bb35ff03fc36bb5fda971
Cesar <kliko1989@hotmail.com>
2020-04-15 12:35:53 -0500

    dfvgbhnjm

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -25,6 +25,8 @@ class StockPicking(Model):
     sale_child=fields.Many2one('sale.order')
     tipo=fields.Char(compute='cliente',store=True)
     oculta=fields.Boolean(store=True)
+    estadoRuta=fields.Selection([["borrador","Borrador"],["valido","Confirmado"]])
+
     #documentosDistro = fields.Many2many('ir.attachment', string="Evidencias ")
     #historialTicket = fields.One2many('ir.attachment','res_id',string='Evidencias al ticket',store=True,track_visibility='onchange')
 
