ad2aa85da3f64ab9adf56f09b2157ff03fe1ca79
Cesar <kliko1989@hotmail.com>
2020-04-16 15:59:59 -0500

    cdcv

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -717,6 +717,7 @@ class SolicitudestockInventoryMassAction(TransientModel):
                         inventoty={'inventory_id':id3.id, 'partner_id':'1','product_id':productid.id,'product_uom_id':'1','product_qty':row[2].value, 'location_id':self.almacen.lot_stock_id.id}
                         self.env['stock.inventory.line'].create(inventoty)
                         busqueda=self.env['stock.quant'].search([['product_id','=',productid.id],['location_id','=',self.almacen.lot_stock_id.id]])
+                        _logger.info(str(busqueda))
                         if(busqueda):
                             busqueda.sudo().write({'quantity':row[2].value})
                         if(busqueda==False):
