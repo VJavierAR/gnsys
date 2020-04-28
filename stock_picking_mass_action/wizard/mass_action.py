170c203ee26b2faafb3e6a505d0f042dfc59b64b
Cesar <kliko1989@gmail.com>
2020-04-28 17:25:08 -0500

    kgjjg

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -893,6 +893,6 @@ class  DevolverPick(TransientModel):
     picking=fields.Many2one('stock.picking')
 
     def confirmar(self):
-        _logger.info(self.picking.default_get(fields))
+        _logger.info(self.picking.default_get(self.picking))
         
         
