0d88952436baad85fb7538783741ddf170358cf3
Cesar <kliko1989@hotmail.com>
2020-04-30 13:39:17 -0500

    hjnkmk,l.ñ-

--- fac_order/models/models.py
+++ fac_order/models/models.py
@@ -332,7 +332,9 @@ class fac_order(models.Model):
         ctx={}
         if(self.x_studio_tipo_de_solicitud):
           if(self.x_studio_tipo_de_solicitud=="Retiro"):
-            self.order_line.context.update({'default_x_studio_cliente':self.partner_shipping_id.display_name})
+            ctx = self.env.context.copy()
+            ctx.get('order_line',False)
+            ctx.update({'default_x_studio_cliente':self.partner_shipping_id.display_name})
             #ctx['context']={'order_line': {'default_x_studio_cliente':self.partner_shipping_id.display_name,'default_x_studio_categoria':'Equipo'}}
         return ctx
 
