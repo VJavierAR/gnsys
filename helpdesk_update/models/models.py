da4e51b4728778f8e58b610bb92058b04cb75291
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-10 00:32:44 -0500

    Update models.py

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -774,8 +774,8 @@ class helpdesk_update(models.Model):
                             lineas.append((0, 0, val))
                         lineas.append((0, 0, {'ticketRelacion': int(self.x_studio_id_ticket), 'comentario': ultimoComentario, 'estadoTicket': "Asignado", 'write_uid':  self.env.user.id}))
                         _logger.info("datos lineas: " + str(lineas))
-                    record.sudo().write({'diagnosticos': lineas})
-                    #record.diagnosticos = lineas
+                    #record.sudo().write({'diagnosticos': lineas})
+                    record.diagnosticos = lineas
                     #self.diagnosticos = [(6, 0, [])]
                     #self.diagnosticos = lineas
                     #self.sudo().write({'diagnosticos': [(0, 0, {'ticketRelacion': self.x_studio_id_ticket, 'comentario': ultimoComentario, 'estadoTicket': "Asignado", 'write_uid':  self.env.user.name})]})
