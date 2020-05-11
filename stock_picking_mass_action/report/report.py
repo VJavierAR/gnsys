cdeea9d29881fed718670d0803c54f0be1c79d90
cesar <kliko1989@gmail.com>
2020-05-11 13:47:08 -0500

    xcvbnm

--- stock_picking_mass_action/report/report.py
+++ stock_picking_mass_action/report/report.py
@@ -57,7 +57,7 @@ class PartnerXlsx(models.AbstractModel):
                 if(obj.x_studio_coment):
                     sheet.write(i, 17, obj.x_studio_coment, bold)
                 if(obj.x_studio_coment==False):
-                    sheet.write(i, 17, user.write_uid.name if(len(user)==1) else '', user[0].write_uid.name)
+                    sheet.write(i, 17, user.write_uid.name if(len(user)==1) else user[0].write_uid.name,bold)
                 i=i+1
             sheet.add_table('A2:R'+str(i),{'style': 'Table Style Medium 9','columns': [{'header': 'Categoria'},{'header': 'Fecha'},{'header': 'Almacen'},{'header':'Tipo'},{'header': 'Modelo'},{'header': 'No Parte'},{'header': 'Cantidad'},{'header': 'Serie'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Comentario'},{'header': 'Documento Origen'},{'header': 'Numero'},{'header': 'Serie Destino'},{'header': 'Modelo Destino'},{'header': 'Estado'},{'header': 'Delegación'},{'header': 'Usuario'}]})
         if(13 not in partners.mapped('x_studio_field_aVMhn.id')):
@@ -94,10 +94,10 @@ class PartnerXlsx(models.AbstractModel):
                 if(obj.x_studio_coment):
                     sheet.write(i, 16, obj.x_studio_coment, bold)
                 if(obj.x_studio_coment==False):
-                    sheet.write(i, 16, user.write_uid.name if(len(user)==1) else '', user[0].write_uid.name)
+                    sheet.write(i, 16, user.write_uid.name if(len(user)==1) else user[0].write_uid.name,bold)
                 i=i+1
             sheet.add_table('A2:Q'+str(i),{'style': 'Table Style Medium 9','columns': [{'header': 'Categoria'},{'header': 'Fecha'},{'header': 'Almacen'},{'header':'Tipo'},{'header': 'Modelo'},{'header': 'No Parte'},{'header': 'Cantidad'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Comentario'},{'header': 'Documento Origen'},{'header': 'Documento Origen'},{'header': 'Serie Destino'},{'header': 'Modelo Destino'},{'header': 'Estado'},{'header': 'Delegación'},{'header': 'Usuario'}]})
-        #workbook.close()
+        workbook.close()
 
 class ExistenciasXML(models.AbstractModel):
     _name = 'report.existencias.report'
