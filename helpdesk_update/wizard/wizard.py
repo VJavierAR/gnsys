5eb6b149e315389d2ecd695e2b7cde676d8204f4
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-11 22:37:27 -0500

    Update wizard.py

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -163,12 +163,18 @@ class HelpDeskNoValidarConComentario(TransientModel):
         self.listaDeCantidaes = ''
         if self.productosACambiar:
             for producto in self.productosACambiar:
-                if self.listaDeCantidaes != '':
-                    self.listaDeCantidaes = str(self.listaDeCantidaes) + "," + str(producto.x_studio_cantidad_pedida)
-                    self.sudo().write({'listaDeCantidaes': str(self.listaDeCantidaes) + "," + str(producto.x_studio_cantidad_pedida)})
-                else:
-                    self.listaDeCantidaes = str(producto.x_studio_cantidad_pedida)
-                    self.sudo().write({'listaDeCantidaes': str(producto.x_studio_cantidad_pedida)})
+                _logger.info("res producto.x_studio_cantidad_pedida: " + str(producto.x_studio_cantidad_pedida))
+                lista.append(producto.x_studio_cantidad_pedida)
+                _logger.info("res lista: " + str(lista))
+                #if self.listaDeCantidaes != '':
+                    #self.listaDeCantidaes = str(self.listaDeCantidaes) + "," + str(producto.x_studio_cantidad_pedida)
+                    #self.sudo().write({'listaDeCantidaes': str(self.listaDeCantidaes) + "," + str(producto.x_studio_cantidad_pedida)})
+                #else:
+                    #self.listaDeCantidaes = str(producto.x_studio_cantidad_pedida)
+                    #self.sudo().write({'listaDeCantidaes': str(producto.x_studio_cantidad_pedida)})
+        _logger.info("res lista: " + str(lista))
+        for cantidad in lista:
+            self.listaDeCantidaes = str(cantidad) + ","
         _logger.info("res listaDeCantidaes: " + str(self.listaDeCantidaes))
 
         
