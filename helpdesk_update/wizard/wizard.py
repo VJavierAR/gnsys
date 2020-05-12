7cc8b0d8c84f9d2663825063b6fd1b4b4174015a
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-11 21:20:22 -0500

    Update wizard.py

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -120,8 +120,8 @@ class HelpDeskNoValidarConComentario(TransientModel):
         self.serieTexto = self.ticket_id.x_studio_equipo_por_nmero_de_serie[0].name
 
     def _compute_productos(self):
-        #self.productosACambiar = [(6, 0, self.ticket_id.x_studio_productos.ids)]
-        self.write({'productosACambiar': [(6, 0, self.ticket_id.x_studio_productos.ids)]})
+        self.productosACambiar = [(6, 0, self.ticket_id.x_studio_productos.ids)]
+        #self.write({'productosACambiar': [(6, 0, self.ticket_id.x_studio_productos.ids)]})
 
     @api.depends('productosACambiar.x_studio_cantidad_pedida')
     def _compute_lista_de_cantidades(self):
