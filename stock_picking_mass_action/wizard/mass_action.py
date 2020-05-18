55409dff6e5194aa52aa2aff26f88a4f7df51a8a
Cesar <kliko1989@gmail.com>
2020-05-18 08:16:53 +0000

    sdfghjkl

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -725,17 +725,28 @@ class SolicitudestockInventoryMassAction(TransientModel):
                 id3=self.env['stock.inventory'].create({'name':str(self.comentario)+' '+str(self.almacen.name), 'location_id':self.almacen.lot_stock_id.id,'x_studio_field_8gltH':self.almacen.id,'state':'done'})
                 for row_num, row in enumerate(sheet.get_rows()):
                     if(i>0):
+                        ubicacion=None
                         template=self.env['product.template'].search([('default_code','=',str(row[1].value).replace('.0',''))])
                         productid=self.env['product.product'].search([('product_tmpl_id','=',template.id)])
                         quant={'product_id':productid.id,'reserved_quantity':'0','quantity':row[2].value, 'location_id':self.almacen.lot_stock_id.id}
                         inventoty={'inventory_id':id3.id, 'partner_id':'1','product_id':productid.id,'product_uom_id':'1','product_qty':row[2].value, 'location_id':self.almacen.lot_stock_id.id}
+                        if(row[3].ctype!=0):
+                            ubicacion=self.env['x_ubicacion_inventario'].search([('x_name','=',str(row[3].value).replace('.0',''))])
+                        if(len(ubicacion)>0):
+                            inventory['x_studio_field_yVDjd']=ubicacion.id
                         self.env['stock.inventory.line'].create(inventoty)
                         busqueda=self.env['stock.quant'].search([['product_id','=',productid.id],['location_id','=',self.almacen.lot_stock_id.id]])
                         _logger.info(str(busqueda))
                         if(busqueda.id):
-                            busqueda.sudo().write({'quantity':row[2].value})
+                            if(len(ubicacion)>0):
+                                busqueda.sudo().write({'quantity':row[2].value,'x_studio_field_kUc4x':ubicacion.id})                            
+                            else:
+                                busqueda.sudo().write({'quantity':row[2].value})
                         if(busqueda.id==False):
-                            self.env['stock.quant'].sudo().create(quant)
+                            if(len(ubicacion)>0):
+                                quant['x_studio_field_kUc4x']=ubicacion.id
+                            else:
+                                self.env['stock.quant'].sudo().create(quant)
                     i=i+1
             else:
                 raise UserError(_("Archivo invalido"))
