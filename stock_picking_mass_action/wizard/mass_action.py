2a5cf77e43cc8fff7134d683f3942179c2fbdd95
Cesar <kliko1989@gmail.com>
2020-05-19 09:41:33 +0000

    xexe

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -730,7 +730,7 @@ class SolicitudestockInventoryMassAction(TransientModel):
                         inventoty={'inventory_id':id3.id, 'partner_id':'1','product_id':productid.id,'product_uom_id':'1','product_qty':row[2].value, 'location_id':self.almacen.lot_stock_id.id}
                         if(row[3].ctype!=0 and row[3].value!=''):
                             ubicacion=self.env['x_ubicacion_inventario'].search([('x_name','=',str(row[3].value).replace('.0',''))])
-                        if(len(ubicacion)!=None):
+                        if(ubicacion!=None):
                             inventoty['x_studio_field_yVDjd']=ubicacion.id
                         self.env['stock.inventory.line'].create(inventoty)
                         busqueda=self.env['stock.quant'].search([['product_id','=',productid.id],['location_id','=',self.almacen.lot_stock_id.id]])
