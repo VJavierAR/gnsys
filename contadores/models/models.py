513ac024645cf0ba0ba6573085a5070e25b29eac
Cesar <kliko1989@gmail.com>
2020-05-21 06:38:30 +0000

    cfghjk

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -1176,8 +1176,8 @@ class contadores_lines(models.Model):
     destino=fields.Many2one('res.partner')
     contrato1=fields.Many2one('contrato')
     servicio1=fields.Many2one('servicios')
-    #servicioz=fields.Char(related='serie.servicio.name')
-    #contrato=fields.Many2one(related='servicioz.contrato.name',string='Contrato origen')
+    servicioz=fields.Char(related='serie.servicio.name')
+    contrato=fields.Many2one(related='servicioz.contrato.name',string='Contrato origen')
 
 
     @api.onchange('serie')
