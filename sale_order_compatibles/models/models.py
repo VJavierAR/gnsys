a60a8dbaad5f087933dc863007e2e4b6d34b89ab
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-24 18:29:43 -0500

    as

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -10,14 +10,14 @@ _logger = logging.getLogger(__name__)
 
 
 class sale_order_compatibles(models.Model):
-	_name = 'sale_order_compatibles.sale_order_compatibles'
+	_name = 'sale_order_compatibles'
 	equipos = fields.One2many('stock.production.lot', 'sale_order_compatible', string = 'Equipos')
 	componentes = fields.Many2many('product.product', string = "Componentes")
 	toner = fields.Many2many('product.product', string = "Toner")
 	compatibles = fields.Many2many('product.product', string = "Compatibles")
 
-	
+
 
 class sale_order_update(models.Model):
 	_inherit = 'stock.production.lot'
-	sale_order_compatible = fields.Many2one('sale_order_compatibles.sale_order_compatibles')
+	sale_order_compatible = fields.Many2one('sale_order_compatibles')
