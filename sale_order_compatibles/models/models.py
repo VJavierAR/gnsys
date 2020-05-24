4ae15b0fe1b6d9f71354e57b045bcf40f5dca323
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-24 18:40:11 -0500

    Update models.py

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -12,7 +12,7 @@ _logger = logging.getLogger(__name__)
 class sale_order_compatibles(models.Model):
 	_name = 'sale_order_compatibles'
 	saleOrder = fields.Many2one('compatibles')
-	equipos = fields.One2many('stock.production.lot', 'sale_order_compatible', string = 'Equipos')
+	equipos = fields.One2many('stock.production.lot', 'sale_order_compatibles', string = 'Equipos')
 	componentes = fields.Many2many('product.product', string = "Componentes")
 	toner = fields.Many2many('product.product', string = "Toner")
 	compatibles = fields.Many2many('product.product', string = "Compatibles")
@@ -21,7 +21,7 @@ class sale_order_compatibles(models.Model):
 
 class sale_order_update(models.Model):
 	_inherit = 'stock.production.lot'
-	sale_order_compatible = fields.Many2one('sale_order_compatibles')
+	sale_order_compatibles = fields.Many2one('sale_order_compatibles')
 
 class sale_update(models.Model):
 	_inherit = 'sale.order'
