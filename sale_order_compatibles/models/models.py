2a442e4e56d30cc2787a5a6a0bcc4740e12f56c9
Cesar <kliko1989@gmail.com>
2020-05-25 05:12:05 +0000

    cvbnm,

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -11,43 +11,42 @@ _logger = logging.getLogger(__name__)
 
 class sale_order_compatibles(models.Model):
 	_name = 'sale_order_compatibles'
-	saleOrder = fields.Many2one('compatibles')
-	equipos = fields.One2many('product.product', 'sale_order_compatibles_equipos', string = 'Equipos')
-	componentes = fields.One2many('product.product', 'sale_order_compatibles_componentes', string = "Componentes")
+	saleOrder = fields.Many2one('sale.order')
+	equipos = fields.Many2one('product.product', 'sale_order_compatibles_equipos', string = 'Equipos')
+	cantidad = fields.Integer(string = 'Cantidad')
+	#componentes = fields.One2many('product.product', 'sale_order_compatibles_componentes', string = "Componentes")
 	
-	toner = fields.Many2one('product.product', string = "Toner")
-	accesorios = fields.Many2one('product.product', string = "Accesorios")
-	compatibles = fields.Many2one('product.product', string = "Compatibles")
-
+	#toner = fields.Many2one('product.product', string = "Toner")
+	#ccesorios = fields.Many2one('product.product', string = "Accesorios")
+	#compatibles = fields.Many2one('product.product', string = "Compatibles")
 	estado = fields.Selection(selection = [('1', '1'),('2', '2'),('3','3')], widget = "statusbar", default = '1')
-
+	componentes=fields.One2many('sale_order_compatibles_mini', 'saleOrderMini', string = 'nombre temp')
 
 
 
 
 class miniModelo(models.Model):
 	_name = 'sale_order_compatibles_mini'
-
 	idProducto = fields.Integer(string = 'id Producto')
-
-	producto = fields.One2many('product.product', 'sale_order_compatibles_mini_producto', string = 'Equipos')
+	producto = fields.Many2one('product.product')
 	cantidad = fields.Integer(string = 'Cantidad')
+	saleOrderMini=fields.Many2one('sale_order_compatibles')
 		
 
 
 
 
-class product_update(models.Model):
-	_inherit = 'product.product'
-	sale_order_compatibles_equipos = fields.Many2one('sale_order_compatibles')
-	sale_order_compatibles_componentes = fields.Many2one('sale_order_compatibles')
+# class product_update(models.Model):
+# 	_inherit = 'product.product'
+# 	sale_order_compatibles_equipos = fields.Many2one('sale_order_compatibles')
+# 	sale_order_compatibles_componentes = fields.Many2one('sale_order_compatibles')
 
 
-	sale_order_compatibles_toner = fields.One2many('sale_order_compatibles', 'toner')
-	sale_order_compatibles_accesorios = fields.One2many('sale_order_compatibles', 'accesorios')
-	sale_order_compatibles_compatibles = fields.One2many('sale_order_compatibles', 'compatibles')
+# 	sale_order_compatibles_toner = fields.One2many('sale_order_compatibles', 'toner')
+# 	sale_order_compatibles_accesorios = fields.One2many('sale_order_compatibles', 'accesorios')
+# 	sale_order_compatibles_compatibles = fields.One2many('sale_order_compatibles', 'compatibles')
 
-	sale_order_compatibles_mini_producto = fields.One2many('sale_order_compatibles_mini', 'producto')
+# 	sale_order_compatibles_mini_producto = fields.One2many('sale_order_compatibles_mini', 'producto')
 
 
 class sale_update(models.Model):
@@ -55,6 +54,6 @@ class sale_update(models.Model):
 	compatiblesLineas = fields.One2many('sale_order_compatibles', 'saleOrder', string = 'nombre temp')
 
 
-class sale_order_update(models.Model):
-	_inherit = 'stock.production.lot'
-	sale_order_compatibles = fields.Many2one('sale_order_compatibles')
+# class sale_order_update(models.Model):
+# 	_inherit = 'stock.production.lot'
+# 	sale_order_compatibles = fields.Many2one('sale_order_compatibles')
