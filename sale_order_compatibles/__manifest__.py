34a3f91ee937660fc23d43cfbb5e5f4cfe565e74
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-24 20:58:15 -0500

    Update __manifest__.py

--- sale_order_compatibles/__manifest__.py
+++ sale_order_compatibles/__manifest__.py
@@ -22,7 +22,7 @@
     # any module necessary for this one to work correctly
     'depends': [
         'base',
-        'stock.production.lot'    
+        'stock'    
     ],
 
     # always loaded
