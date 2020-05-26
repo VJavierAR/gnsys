465d9fbc2a0f6339df30bdc32bd7f5de06d08ba6
Cesar <kliko1989@gmail.com>
2020-05-26 14:54:20 -0500

    dfghjk

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -388,7 +388,7 @@ class StockPicking(Model):
             data={'move_id':p.id,'rel_cambio':wiz.id,'producto1':p.product_id.id,'producto2':p.product_id.id,'cantidad':p.product_uom_qty,'serie':p.x_studio_serie_destino.id,'tipo':self.picking_type_id.id}
             self.env['cambio.toner.line.toner'].create(data)
 
-        for p in self.move_ids_without_package.filtered(lambda x:x.product_id.categ_id.id==9):
+        for p in self.move_ids_without_package.filtered(lambda x:x.product_id.categ_id.id==11):
             data={'move_id':p.id,'rel_cambio':wiz.id,'producto1':p.product_id.id,'producto2':p.product_id.id,'cantidad':p.product_uom_qty,'serie':p.x_studio_serie_destino.id,'tipo':self.picking_type_id.id}
             self.env['cambio.toner.line.accesorios'].create(data)
         
