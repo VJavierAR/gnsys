92258e66eb2de469148643397e6c11a6d34195fb
Cesar <kliko1989@gmail.com>
2020-05-27 14:15:08 -0500

    vfdvgfd

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -13,7 +13,7 @@ class sale_order_compatibles(models.Model):
 	_name = 'sale_order_compatibles'
 	saleOrder = fields.Many2one('sale.order')
 	equipos = fields.Many2one('product.product', string = 'Equipos')
-	cantidad = fields.Integer(string = 'Cantidad')
+	cantidad = fields.Integer(string = 'Cantidad',default=1,readonly=True)
 	estado = fields.Selection(selection = [('1', '1'),('2', '2'),('3','3')], widget = "statusbar", default = '1')
 	componentes = fields.One2many('sale_order_compatibles_mini', 'saleOrderMini', string = 'Componentes')
 	toner = fields.One2many('sale_order_compatibles_mini_toner', 'saleOrderMini', string = 'Toner')
@@ -41,8 +41,6 @@ class miniModelo(models.Model):
 			da=self.env['product.product'].browse(eval(self.idProducto)).filtered(lambda x:x.categ_id.id==7).mapped('id')
 			res['domain']={'producto':[['id','in',da]]}
 		return res
-
-
 
 
 class miniModeloToner(models.Model):
@@ -75,32 +73,6 @@ class miniModeloAccesorio(models.Model):
 			da=self.env['product.product'].browse(eval(self.idProducto)).filtered(lambda x:x.categ_id.id==11).mapped('id')
 			res['domain']={'producto':[['id','in',da]]}
 		return res
-
-
-	
-	"""
-	@api.onchange('idProducto')
-	def dominioProducto(self):
-		dominio = [('categ_id', '=', self.idProducto)]
-		res['domain'] = {'producto': dominio}
-		return res
-	"""
-
-
-
-
-# class product_update(models.Model):
-# 	_inherit = 'product.product'
-# 	sale_order_compatibles_equipos = fields.Many2one('sale_order_compatibles')
-# 	sale_order_compatibles_componentes = fields.Many2one('sale_order_compatibles')
-
-
-# 	sale_order_compatibles_toner = fields.One2many('sale_order_compatibles', 'toner')
-# 	sale_order_compatibles_accesorios = fields.One2many('sale_order_compatibles', 'accesorios')
-# 	sale_order_compatibles_compatibles = fields.One2many('sale_order_compatibles', 'compatibles')
-
-# 	sale_order_compatibles_mini_producto = fields.One2many('sale_order_compatibles_mini', 'producto')
-
 
 class sale_update(models.Model):
 	_inherit = 'sale.order'
@@ -148,8 +120,3 @@ class sale_update(models.Model):
 			'target': 'new',
 			'context': ctx,
 		}
-
-
-# class sale_order_update(models.Model):
-# 	_inherit = 'stock.production.lot'
-# 	sale_order_compatibles = fields.Many2one('sale_order_compatibles')
