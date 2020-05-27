d6638c8ee492b1e57ed7b49095961b0061960cb7
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-27 18:46:45 -0500

    Update models.py

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -2034,7 +2034,7 @@ class helpdesk_update(models.Model):
                     query = """insert into sale_order 
                                 (partner_id, origin, x_studio_tipo_de_solicitud, x_studio_requiere_instalacin, user_id, \"x_studio_tcnico\", \"x_studio_field_RnhKr\", partner_shipping_id, warehouse_id, team_id, x_studio_comentario_adicional, x_studio_field_bxHgp, x_studio_corte)
                                 values (""" + str(record.partner_id.id) + """
-                                        , """ + str(id_ticket) + """
+                                        , '""" + str(id_ticket) + """'
                                         , """ + "Venta"  + """
                                         , """ + """t""" + """
                                         , """ + str(record.user_id.id) + """
@@ -2050,7 +2050,7 @@ class helpdesk_update(models.Model):
                     query = """insert into sale_order 
                                 (partner_id, origin, x_studio_tipo_de_solicitud, x_studio_requiere_instalacin, user_id, \"x_studio_tcnico\", \"x_studio_field_RnhKr\", partner_shipping_id, warehouse_id, team_id, x_studio_comentario_adicional, x_studio_field_bxHgp, x_studio_corte)
                                 values (""" + str(record.partner_id.id) + """
-                                        , """ + str(id_ticket) + """
+                                        , '""" + str(id_ticket) + """'
                                         , """ + "Venta"  + """
                                         , """ + """t""" + """
                                         , """ + str(record.user_id.id) + """
