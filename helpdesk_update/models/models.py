099b44cd83446d5dadc9010c2d485223da130a58
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-27 17:04:24 -0500

    Update models.py

--- helpdesk_update/models/models.py
+++ helpdesk_update/models/models.py
@@ -2028,6 +2028,7 @@ class helpdesk_update(models.Model):
                 _logger.info("aaaa: " + str(self.x_studio_corte))
                 corte = str(self.x_studio_corte)
                 _logger.info("aaaa: " + corte)
+                _logger.info("aaaa: " + str(record.x_studio_id_ticket)str(record.x_studio_id_ticket))
                 if corte == 'False':
                     query = """insert into sale_order 
                                 (partner_id, origin, x_studio_tipo_de_solicitud, x_studio_requiere_instalacin, user_id, x_studio_tcnico, x_studio_field_RnhKr, partner_shipping_id, warehouse_id, team_id, x_studio_comentario_adicional, x_studio_field_bxHgp, x_studio_corte)
