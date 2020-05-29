d621f6685a19790d99b19ddf8eab25b74e6027c1
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-05-29 14:51:32 -0500

    Update models.py

--- h_cliente/models/models.py
+++ h_cliente/models/models.py
@@ -12,20 +12,20 @@ _logger = logging.getLogger(__name__)
 class HCliente(models.Model):
 	_name = 'HCliente'
 	_description = 'Clientes de almacen'
-
+	
 	fecha = fields.Datetime(string = 'Fecha')
 
-    origen = fields.Text(string = 'Origen')
-    destino = fields.Text(string = 'Destino')
-    localidad = fields.Text(string = 'Localidad')
+	origen = fields.Text(string = 'Origen')
+	destino = fields.Text(string = 'Destino')
+	localidad = fields.Text(string = 'Localidad')
 	contadorBNPag = fields.Text(string = 'Contador BN Pag.')
 	contadorColorPag = fields.Text(string = 'Contador Color Pag.')
 	contadorBNML = fields.Text(string = 'Contador BN ML')
 	contadorColorML = fields.Text(string = 'Contador Color ML')
+
 	causa = fields.Text(string = 'Causa')
 
 	serie = fields.Many2one('stock.production.lot', string = 'Serie')
-	
 
 class SeriesUpdate(object):
 	_inherit = 'stock.production.lot'
