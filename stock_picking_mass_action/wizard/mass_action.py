24028bf19c5a7267ed8a9ee15b425a7ecb18fc38
cesar <kliko1989@gmail.com>
2020-05-31 00:57:50 -0500

    rtyujk

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -17,7 +17,6 @@ import logging, ast
 from odoo.tools import config, DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, pycompat
 _logger = logging.getLogger(__name__)
 
-
 try:
     import xlrd
     try:
