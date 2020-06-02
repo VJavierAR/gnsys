d394bc33071caa24b181d85aefaf5f7fd8bd1885
Cesar <kliko1989@hotmail.com>
2020-06-02 13:45:06 -0500

    rgv

--- compras/models/models.py
+++ compras/models/models.py
@@ -100,7 +100,7 @@ class compras(models.Model):
                         fff=open("tt.txt","w")
                         for t in text:
                             if('H87 -' in t):
-                                tt=t.split('H87 -')[1].split('$')
+                                tt=t.split('H87 -').split('$')
                                 fff.write(str(tt))
                         fff.close()
                     if(self.archivo and ("konica" in self.partner_id.name.lower() or "kyocera" in self.partner_id.name.lower())):
