66e012f3220d6db9d3465bb5d953aaf925f98e0c
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2020-06-02 11:35:54 -0500

    asas

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -651,6 +651,7 @@ class contadores(models.Model):
                         worksheet.write(i, 11, ec)
                         
                         if rpt.x_studio_color_o_bn=='B/N':
+                            
                            if rd.bolsaBN<ebn:
                               ebn=ebn-rd.bolsaBN
                               eebn=ebn+eebn  
@@ -661,16 +662,7 @@ class contadores(models.Model):
                               worksheet.write(i, 14,'$ '+str(iva +cal) )  
                               ivatt=iva+ivatt  
                               totalsr=(float(rd.rentaMensual)+(ebn*rd.clickExcedenteBN))+totalsr
-                              ttotal=(iva +cal)+ttotal
-                           else:                                                            
-                              cal=float(rd.rentaMensual)
-                              worksheet.write(i, 12, cal)
-                              iva=round(cal*.16,2)
-                              worksheet.write(i, 13,'$ '+str(iva) )
-                              worksheet.write(i, 14,'$ '+str(iva +cal) )  
-                              ivatt=iva+ivatt  
-                              totalsr=(float(rd.rentaMensual)+totalsr
-                              ttotal=(iva +cal)+ttotal  
+                              ttotal=(iva +cal)+ttotal                                       
                         if rpt.x_studio_color_o_bn=='Color':
                            if rd.bolsaBN<ebn:
                               ebn=ebn-rd.bolsaBN
@@ -687,18 +679,7 @@ class contadores(models.Model):
                               worksheet.write(i, 14,'$ '+str(iva +call) )
                               totalsr=call+totalsr
                               ttotal=(iva +call)+ttotal
-                           else:
-                              ec=ec-rd.bolsaColor
-                              eec=ec+eec  
-                              call=float(rd.rentaMensual)                                
-                              worksheet.write(i, 12, call)
-                              iva=round(call*.16,2)
-                              ivatt=iva+ivatt
-                              worksheet.write(i, 13,'$ '+str(iva) )     
-                              worksheet.write(i, 14,'$ '+str(iva +call) )
-                              totalsr=call+totalsr
-                              ttotal=(iva +call)+ttotal          
-                                       
+                                                 
                         i=i+1   
                     if rd.nombreAnte=='Costo por página procesada BN o color':
                         worksheet.write(i, 0, rpt.x_studio_indice)
