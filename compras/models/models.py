13548f2ad4165d40a302318e374f3fc8536313e3
cesar <kliko1989@gmail.com>
2020-06-04 05:59:41 -0500

    xsxs

--- compras/models/models.py
+++ compras/models/models.py
@@ -102,7 +102,7 @@ class compras(models.Model):
                             if('Pieza' in ar):
                                 cantidad=float(ar.split('      ',1)[1].split('        ',1)[0].replace(' ',''))
                                 noparte=ar.split('      ',1)[1].split('        ',1)[1].split('            ',1)[0].split(str(ar.split('      ',1)[1].split('        ',1)[0].replace(' ','')),1)[1]
-                                #_logger.info(str(noparte))
+                                _logger.info(str(noparte))
                                 p=ar.split('$')
                                 precio=float(p[1].replace(' ',''))
                                 descuento=float(p[4].replace(' ','')) if(len(p)==5) else 0
