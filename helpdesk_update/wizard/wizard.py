d5c17e014c97364123112f32934b42790f2bc5a2
Marco <47366939+VJavierAR@users.noreply.github.com>
2020-06-06 11:55:50 -0500

    Update wizard.py

--- helpdesk_update/wizard/wizard.py
+++ helpdesk_update/wizard/wizard.py
@@ -1951,14 +1951,15 @@ class HelpDeskDetalleSerieToner(TransientModel):
                                 string = 'Series',
                                 #default = lambda self: self._default_serie_ids(),
                             )
+
     
-    @api.one
     def _default_dominio(self):
         ids = []
         _logger.info('hola2: ' + str(self.ticket_id.x_studio_equipo_por_nmero_de_serie_1))
-        for dca in self.ticket_id.x_studio_equipo_por_nmero_de_serie_1:
-            ids.append(dca.serie.id)
-        ids = str(self._context['dominio'])
+        #for dca in self.ticket_id.x_studio_equipo_por_nmero_de_serie_1:
+        #    ids.append(dca.serie.id)
+        #ids = str(self._context['dominio'])
+        ids = str(self.env.context.get('dominio'))
         return str(ids)
 
     dominio = fields.Text(string = 'Dominio', store = True, default = lambda self: self._default_dominio())
