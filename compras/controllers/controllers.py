410b9318f47e8253339e11cde214bfd1e376aac1
cesar <kliko1989@gmail.com>
2020-06-08 00:45:29 -0500

    ssss

--- compras/controllers/controllers.py
+++ compras/controllers/controllers.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from datetime import date
+from odoo import fields, http, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.mail import _message_post_helper
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.osv import expression
 
 class Compras(http.Controller):
-    @http.route('/compras/compras/<int:order_id>', auth='public')
-    def fun(self,order_id,**kw):
+    @http.route('/compras/compras/<int:order_id>', type='http', auth="public", website=True)
+    def fun(self,order_id, report_type=None, access_token=None, message=False, download=False, **kw):
     	p=self.env['purchase.order'].search([['id','=',order_id]])
     	p.button_confirm()
     	return "Solicitud"+str(p.name)+"Autorizada"
