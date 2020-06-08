52d9b9182ad5a7ffec68047585d902b947ec9565
cesar <kliko1989@gmail.com>
2020-06-08 02:09:38 -0500

    ssss

--- compras/controllers/controllers.py
+++ compras/controllers/controllers.py
@@ -5,7 +5,7 @@ class Compras(http.Controller):
     @http.route('/compras/compras/<int:purchase_id>', auth='public')
     def index(self,purchase_id ,**kw):
     	p=self.env['purchase.order'].search([['id','=',purchase_id]])
-        return "Orden de compra"+p.name+" Autorizada"
+    	return "Orden de compra"+str(p.name)+" Autorizada"
 
 #     @http.route('/compras/compras/objects/', auth='public')
 #     def list(self, **kw):
