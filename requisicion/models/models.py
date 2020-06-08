64dc659651fbb45cd9680d2040f527d0946160d8
cesar <kliko1989@gmail.com>
2020-06-08 03:41:09 -0500

    ssss

--- requisicion/models/models.py
+++ requisicion/models/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import logging, ast
+_logger = logging.getLogger(__name__)
 class product_requisicion(models.Model):
     _name = 'product.rel.requisicion'
     _description='Rel requisiocion'
