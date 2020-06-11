3ecb8e0b450c036db30e255f0ab57658e7274dd7
Cesar <kliko1989@hotmail.com>
2020-06-11 17:13:56 -0500

    gfgf

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -145,7 +145,7 @@ class StockPickingMassAction(TransientModel):
                     for move in pick_to_backorder.move_lines:
                         if float_compare(move.product_uom_qty, move.quantity_done, precision_rounding=move.product_uom.rounding) > 0:
                             moves_to_log[move] = (move.quantity_done, move.product_uom_qty)
-                        pick_id._log_less_quantities_than_expected(moves_to_log)
+                        pick_to_backorder._log_less_quantities_than_expected(moves_to_log)
                 self.picking_ids.action_done()
                 if cancel_backorder:
                     backorder_pick = self.env['stock.picking'].search([('backorder_id', '=', pick_to_backorder.id)])
