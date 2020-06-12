85bc5ef9885e46d930a3bdecac976563e97ff289
cesar <kliko1989@gmail.com>
2020-06-12 00:37:29 -0500

    xasxsax

--- compras/wizard/wizard.py
+++ compras/wizard/wizard.py
@@ -30,5 +30,33 @@ class PurchaseAssig(models.Model):
 	purchase_ids=fields.Many2many('purchase.order',default=lambda self: self._default_purchase_ids(),)
 	
 	def confirmar(self):
-		p=self.purchase_ids.write({'x_studio_autoriza':self.autoriza,'x_studio_area_de_solicitud':self.area,'x_studio_solicita':self.solicitante})
-		return self.env.ref('compras.report_custom_purchase').report_action(p)
+		#p=self.purchase_ids.write({'x_studio_autoriza':self.autoriza,'x_studio_area_de_solicitud':self.area,'x_studio_solicita':self.solicitante})
+		return self.env.ref('compras.report_custom_purchase').report_action(self.purchase_ids)
+
+
+class PurchaseOrderConfirm(models.TransientModel):
+    _name = 'purchase.order.confirm'
+    _description = "Wizard - Purchase Order Confirm/Cancel"
+
+    @api.multi
+    def purchase_confirm(self):
+        """filter the records of the state 'draft' and 'sent',
+        and will confirm this and others will be skipped"""
+        quotations = self._context.get('active_ids')
+        quotations_ids = self.env['purchase.order'].browse(quotations).\
+            filtered(lambda x: x.state == 'draft' or x.state == "sent")
+        quotations_ids1 = self.env['purchase.order'].browse(quotations).\
+            filtered(lambda x: x.state == 'to approve')
+        if(len(quotations_ids)> 0):
+        	quotations_ids.button_confirm()
+        if(len(quotations_ids1)> 0):
+        	quotations_ids1.button_approve()
+
+    @api.multi
+    def purchase_cancel(self):
+        quotations = self._context.get('active_ids')
+        quotations_ids = self.env['purchase.order'].browse(quotations)
+        try:
+            quotations_ids.button_cancel()
+        except:
+            pass
