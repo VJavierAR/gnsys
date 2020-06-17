c6e4eb536ff80a630cd0f1a62801dbc2ea12bc70
Cesar <kliko1989@gmail.com>
2020-06-17 17:43:25 -0500

    gergre

--- compras/models/models.py
+++ compras/models/models.py
@@ -351,7 +351,9 @@ class compras(models.Model):
             'type': 'in_invoice',
             'purchase_id': self.id,
             'currency_id': self.currency_id.id,
-            'company_id': self.company_id.id}
+            'company_id': self.company_id.id,
+            'partner_id':self.partner_id.id
+            'invoice_lines_ids':self.order_line}
             self.env['account.invoice'].create(result)
         if(len(self.x_studio_field_H9kGQ)==1):
             self.action_view_invoice()
