bdc892b1b62a76f669b629865f20e02f048ff8bb
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-06-21 19:13:58 -0500

    asdfghjk

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -222,14 +222,17 @@ class motivos_gastos(models.Model):
 class comprobaciones(models.Model):
     _name = 'gastos.comprobaciones'
     _description = 'Tipos de comprobaciónes del gasto'
-    nombre                  = fields.Char(string="Nombre de comprobación", track_visibility='onchange')
     comprobante             = fields.Many2one('gastos', string = "Comprobación ", track_visibility='onchange')
+
+
     concepto                = fields.Text(string = "Concepto",      track_visibility='onchange')
     descripcion             = fields.Text(string = "Descripción",   track_visibility='onchange')
     justificacon            = fields.Text(string = "Justificación", track_visibility='onchange')
-    monto                   = fields.Float(string = "Monto",         track_visibility='onchange')
-    comprobantes            = fields.Many2many('ir.attachment', string="Comprobantes")
     tipoDeComprobante       = fields.Selection((('Factura','Factura'),('FacturaSinIva','Factura sin IVA'),('TiketFacturable','Ticket facturable'),('Tiket','Ticket'),('Nota','Nota')), string = "Tipo de Comprobante",track_visibility='onchange')
+    comprobantes            = fields.Many2many('ir.attachment', string="Comprobantes")
+    nombre                  = fields.Char(string="Nombre de comprobación", track_visibility='onchange')
+    # -------------------
+    monto                   = fields.Float(string = "Monto",         track_visibility='onchange')
     porcentajeAceptado      = fields.Selection((('100','100%'),('75','75%'),('50','50%'),('25','25%'),('0','0%')), string = "Porcentaje Aceptado",track_visibility='onchange')
     montoJustificado        = fields.Float(string = 'Monto aprobado', compute='calcularMontoAprobado', track_visibility='onchange')
     cuentaContableDestino   = fields.Text(string = "Aplicación contable", track_visibility='onchange')
@@ -263,8 +266,6 @@ class PagoSolicitante(models.Model):
     # responsableDeMontoAjustado = fields.Many2one('res.users', string = "Responsable de monto ajustado", track_visibility='onchange')
     # complementoDePagoPorHacer = fields.Float(string = "Complemento de pago por hacer")
     # devolucionPorRecuperar = fields.Float(string = "Devolución por recuperar")
-
-
     # def calcularSaldo(self):
     #     for rec in self:
     #         rec.saldo = rec.montoEntregado - rec.montoJustificado
