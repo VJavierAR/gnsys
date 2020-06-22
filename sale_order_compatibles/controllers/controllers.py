8ce2dddcd70cce0ee91b70e1fb6b5c641c38c5e1
cesar <kliko1989@gmail.com>
2020-06-22 04:22:36 -0500

    rtyu

--- sale_order_compatibles/controllers/controllers.py
+++ sale_order_compatibles/controllers/controllers.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class SaleOrderCompatibles(http.Controller):
-#     @http.route('/sale_order_compatibles/sale_order_compatibles/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
 class SaleOrderCompatibles(http.Controller):
-    @http.route('/sale/order/<int:sale_id>', auth='public')
-    def index(self,sale_id ,**kw):
+    @http.route('/sale_order_compatibles/sale_order_compatibles/', auth='public')
+    def index(self, **kw):
         p=request.env['sale.order'].search([['id','=',sale_id]])
         if(p.x_studio_tipo_solicitud in ["Venta","Venta directa","Arrendamiento"]):
             p.action_confirm()
@@ -16,6 +12,17 @@ class SaleOrderCompatibles(http.Controller):
         if(p.x_studio_tipo_solicitud == "Retiro"):
             p.retiro()
         return "Orden  "+str(p.name)+" Autorizada"
+# class SaleOrderCompatibles(http.Controller):
+#     @http.route('/sale/order/<int:sale_id>', auth='public')
+#     def index(self,sale_id ,**kw):
+#         p=request.env['sale.order'].search([['id','=',sale_id]])
+#         if(p.x_studio_tipo_solicitud in ["Venta","Venta directa","Arrendamiento"]):
+#             p.action_confirm()
+#         if(p.x_studio_tipo_solicitud == "Cambio"):
+#             p.cambio()
+#         if(p.x_studio_tipo_solicitud == "Retiro"):
+#             p.retiro()
+#         return "Orden  "+str(p.name)+" Autorizada"
 
 
 
