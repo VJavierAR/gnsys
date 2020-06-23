c64994d1a46c1fb202934d3fbcb1fc14a7713cf6
cesar <kliko1989@gmail.com>
2020-06-23 03:47:19 -0500

    trg5ftgvf

--- sale_order_compatibles/controllers/controllers.py
+++ sale_order_compatibles/controllers/controllers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 
 class SaleOrderCompatibles(http.Controller):
     @http.route('/sale_order_compatibles/sale_order_compatibles/<int:sale_id>', auth='public')
