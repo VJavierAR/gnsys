f206e6b6d02d589be1c171818b83c0cd85afdb22
cesar <kliko1989@gmail.com>
2020-06-30 02:43:34 -0500

    fcxszadcf

--- requisicion/models/models.py
+++ requisicion/models/models.py
@@ -42,6 +42,7 @@ class requisicion(models.Model):
     picking_ids=fields.Many2many('stock.picking','picking_req_rel','picking_id','req_id')
     proveedor=fields.Many2one('res.partner')
     ordenes=fields.Many2many('purchase.order')
+    pedido=self.Char()
     @api.one
     def update_estado(self):
         self.write({'state':'open'})
@@ -52,7 +53,9 @@ class requisicion(models.Model):
     def update_estado1(self):
         p=[]
         d=[]
+        sols=[]
         cadena=""
+        sol=""
         if(self.area=="Almacen"):
             _logger.info(str(self.product_rel.mapped('product.x_studio_field_7aUDq.id')))
             pp=self.product_rel.filtered(lambda x:x.cantidad!=0)
@@ -73,6 +76,7 @@ class requisicion(models.Model):
                                 t=t+hi.cantidad
                             #e.write({'pedido':ordenDCompra.name})
                             lineas=self.env['purchase.order.line'].sudo().create({'name':prod.product.description if(prod.product.description) else '|','product_id':prod.product.id,'product_qty':t,'price_unit':prod.costo,'taxes_id':[10],'order_id':ordenDCompra.id,'date_planned':self.fecha_prevista if(self.fecha_prevista) else datetime.datetime.now(),'product_uom':'1'})
+                            sols.append({'product_id':prod.product.id,'cantidad':t})                            
                             d.append(prod.product.id)
             ppp=pp.filtered(lambda x: x.product.x_studio_field_7aUDq.id==False)
             if(len(ppp)>0):
@@ -88,6 +92,7 @@ class requisicion(models.Model):
                             t=t+hi.cantidad
                         #e.write({'pedido':ordenDCompra.name})
                         lineas=self.env['purchase.order.line'].sudo().create({'name':prod.product.description if(prod.product.description) else '|','product_id':prod.product.id,'product_qty':t,'price_unit':prod.costo,'taxes_id':[10],'order_id':ordenDCompra.id,'date_planned':self.fecha_prevista if(self.fecha_prevista) else datetime.datetime.now(),'product_uom':'1'})
+                        sols.append({'product_id':prod.product.id,'cantidad':t})                            
                         d.append(prod.product.id)
         if(self.area!='Almacen'):
             ordenDCompra=self.env['purchase.order'].sudo().create({'partner_id':self.proveedor.id,'date_planned':self.fecha_prevista if(self.fecha_prevista) else datetime.datetime.now(),'x_studio_field_a4rih':'General'})
@@ -95,7 +100,9 @@ class requisicion(models.Model):
             cadena=cadena+ordenDCompra.name
             for hi in self.product_rel:
                 lineas=self.env['purchase.order.line'].sudo().create({'name':hi.product.description if(hi.product.description) else '|','product_id':hi.product.id,'product_qty':hi.cantidad,'price_unit':hi.costo/1.16,'taxes_id':[10],'order_id':ordenDCompra.id,'date_planned':self.fecha_prevista if(self.fecha_prevista) else datetime.datetime.now(),'product_uom':'1'})
+                sols.append({'product_id':hi.product.id,'cantidad':hi.cantidad})                            
         self.ordenes=p
+        self.pedido=str(sols)
         self.write({'state':'done'})
         self.orden=cadena
         # if(self.orden==False):
@@ -119,6 +126,10 @@ class requisicion(models.Model):
         #         if(ot==0):
         #             self.write({'state':'done'})
         #         record['orden']=self.orden+','+ordenDCompra.name
+
+    @api.onchange('ordenes')
+    def calculaCambio(self):
+
 
     @api.model
     def create(self,vals):
