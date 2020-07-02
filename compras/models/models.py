1567d23d4673e31f076b333cf2fb2891c503ed4d
Cesar <kliko1989@gmail.com>
2020-07-02 18:08:35 -0500

    fghjklaga

--- compras/models/models.py
+++ compras/models/models.py
@@ -312,7 +312,8 @@ class compras(models.Model):
                                         product['name']=desc
                                         arr.append(product)
                                     i=i+1       
-                                if('Customer' in o or 'SUPPLY' in o or 'PARTS' in o):
+                                #if('Customer' in o or 'SUPPLY' in o or 'PARTS' in o):
+                                if('PARTS' in o and '$' in o):
                                     s = o.split("$")
                                     h=float(s[2])
                                     g=float(s[1].split(' ')[0])
