667c786fcf7cbf0c4f73366a00b4e9337f2894d4
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-07-09 04:31:42 -0500

    poiuyt

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -365,7 +365,7 @@ class comprobaciones(models.Model):
     tipoDeComprobante       = fields.Selection((('Factura','Factura'),('FacturaSinIva','Factura sin IVA'),('TiketFacturable','Ticket facturable'),('Tiket','Ticket'),('Nota','Nota')), string = "Tipo de Comprobante",track_visibility='onchange')
     comprobantes            = fields.Many2many('ir.attachment', string="Evidencia")
     # -------Finanzas------------
-    porcentajeAceptado      = fields.Percent(string = "Porcentaje Aceptado",track_visibility='onchange')
+    porcentajeAceptado      = fields.Float(string = "Porcentaje Aceptado",track_visibility='onchange')
     montoAprobado           = fields.Float(string = "Monto aprobado", compute='calcularMontoAprobado',track_visibility='onchange')
     cuentaContableDestino   = fields.Text(string = "Aplicación contable", track_visibility='onchange')
     montoAprobadooriginalMante = fields.Float(string = "Monto aprobado originalmente", track_visibility='onchange')
@@ -382,7 +382,7 @@ class comprobaciones(models.Model):
     def calcularMontoAprobado(self):
         for rec in self:
             if rec.porcentajeAceptado :
-                self.montoAprobado = rec.monto * rec.porcentajeAceptado
+                self.montoAprobado = rec.monto * (rec.porcentajeAceptado/100)
 
 
 class PagoSolicitante(models.Model):
