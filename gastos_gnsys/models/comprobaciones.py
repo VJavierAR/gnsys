83f4e916ebde9d378f3999877dd75194f9bb3b45
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-07-10 19:23:11 -0500

    ghnm

--- gastos_gnsys/models/comprobaciones.py
+++ gastos_gnsys/models/comprobaciones.py
@@ -20,13 +20,13 @@ class comprobaciones(models.Model):
     comprobantes            = fields.Many2many('ir.attachment', string="Comprobantes")
     tipoDeComprobante       = fields.Selection((('Factura','Factura'),('FacturaSinIva','Factura sin IVA'),('TiketFacturable','Ticket facturable'),('Tiket','Ticket'),('Nota','Nota')), string = "Tipo de Comprobante",track_visibility='onchange')
     porcentajeAceptado      = fields.Selection((('100','100%'),('75','75%'),('50','50%'),('25','25%'),('0','0%')), string = "Porcentaje Aceptado",track_visibility='onchange')
-    montoJustificado        = fields.Float(string = 'Monto aprobado', compute='calcularMontoAprobado', track_visibility='onchange')
+    # montoJustificado        = fields.Float(string = 'Monto aprobado', compute='calcularMontoAprobado', track_visibility='onchange')
     cuentaContableDestino   = fields.Text(string = "Aplicación contable", track_visibility='onchange')
     centoDeCostos           = fields.Text(string = "Centro de Costos", track_visibility='onchange')
     cliente = fields.Many2one('res.partner', string = 'Cliente', track_visibility='onchange')
     servicio = fields.Text(string = 'Servicio')
 
-    def calcularMontoAprobado(self):
-        for rec in self:
-            if str(rec.porcentajeAceptado) != 'false':
-                montoJustificado = rec.monto * rec.porcentajeAceptado
+    # def calcularMontoAprobado(self):
+    #     for rec in self:
+    #         if str(rec.porcentajeAceptado) != 'false':
+    #             montoJustificado = rec.monto * rec.porcentajeAceptado
