0ffaa50d77f68a5c7b20606c41253692f9573155
cesar <kliko1989@gmail.com>
2020-07-13 04:33:45 -0500

    cercef

--- compras/models/models.py
+++ compras/models/models.py
@@ -56,8 +56,8 @@ class compras(models.Model):
     pagos=fields.Char(widget='html')
     active = fields.Boolean('Active', default=True, track_visibility=True)
     requisicion=fields.Many2one('requisicion.requisicion')
-    x_studio_arreglo=fields:Char()
-    
+    x_studio_arreglo=fields.Char()
+
     @api.depends('x_studio_field_H9kGQ','state')
     def pay(self):
         pago=False
