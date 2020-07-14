a4d6ee3d88d8f3ac8ebdb24b23c0ef84f69a4ea6
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-07-13 20:17:32 -0500

    ertre

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -49,7 +49,10 @@ class gastos_gnsys(models.Model):
             esMenor = "Es menor"
             esMayor = "Es mayor"
             if fecha1 <= fecha2 :
-                self.fechaDeSolicitud = ""
+                for rec in self:
+                    fecha = str(rec.create_date).split(' ')[0]
+                    converted_date = datetime.datetime.strptime(fecha, '%Y/%m/%d').date()
+                    self.fechaLimite = converted_date
                 raise exceptions.ValidationError("El pago no puede ser mayor al día de hoy .")
                 message = ("El pago no puede ser mayor al día de hoy .")
                 mess = { 'title': _('Error'), 'message' : message}
