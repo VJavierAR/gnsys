25c0c3a444230920087705d83f00ef3147433367
Cesar <kliko1989@gmail.com>
2020-07-20 15:05:27 -0500

    sdfghj

--- tfs/controllers/controllers.py
+++ tfs/controllers/controllers.py
@@ -8,14 +8,24 @@ _logger = logging.getLogger(__name__)
 class SaleOrderCompatibles(http.Controller):
     @http.route('/tfs/autoriza/<int:tfs_id>', auth='public')
     def index(self, tfs_id,**kw):
+        mensaje="No cuenta con permisos para relizar esta acción"
         p=request.env['tfs.tfs'].search([['id','=',tfs_id]])
-        p.valida()   
-        return "Proceso  "+str(p.name)+" Autorizado"
+        uido=request.env.context.get('uid')
+        u=request.env['res.groups'].search([['name','=','miniAlmacen']]).users.filtered(lambda x:x.id==uido)
+        if(u.id!=False):
+            p.valida()
+            mensaje="Proceso  "+str(p.name)+" Autorizado"
+        return mensaje
 
 
 class SaleOrderCompatiblesCancel(http.Controller):
     @http.route('/tfs/cancela/<int:tfs_id>', auth='public')
     def index(self, tfs_id,**kw):
+        mensaje="No cuenta con permisos para relizar esta acción"
         p=request.env['tfs.tfs'].search([['id','=',tfs_id]])
-        p.canc()
-        return "Proceso  "+str(p.name)+" Cancelado"
+        uido=request.env.context.get('uid')
+        u=request.env['res.groups'].search([['name','=','miniAlmacen']]).users.filtered(lambda x:x.id==uido)
+        if(u.id!=False):
+            p.canc()
+            mensaje="Proceso  "+str(p.name)+" Cancelado"
+        return mensaje
