712beda0b7281ca3dbb01752f8c14d01c78c80d6
Cesar <kliko1989@gmail.com>
2020-07-22 16:16:48 -0500

    ujkl

--- stock_picking_mass_action/models/quant.py
+++ stock_picking_mass_action/models/quant.py
@@ -30,7 +30,7 @@ class StockQuan(Model):
     #@api.onchange('quantity')
     def actualizaRegla(self):
         if(self.x_studio_almacn.x_studio_mini==True):
-            q=self.env['stock.warehouse.orderpoint'].search([['location_id','=',self.location_id.id],['product_id','=',self.product_id.id],['active','=',False]])
+            q=self.env['stock.warehouse.orderpoint'].search([['location_id','=',self.location_id.id],['product_id','=',self.product_id.id]])
             q.x_studio_existencia=self.quantity
             q.x_studio_existencia_2=self.quantity
 
