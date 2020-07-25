167372e7330341e50988a48a0aca2f2365659da4
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-07-25 16:17:41 -0500

    kkk

--- gastos_gnsys/models/models.py
+++ gastos_gnsys/models/models.py
@@ -50,7 +50,7 @@ class gastos_gnsys(models.Model):
             if not self.montoAutorizado :    
                 self.montoAutorizado = self.montoRequerido
     
-    
+    @api.onchange('autorizacionLider')
     @api.multi
     def gastoAutorizado(self) : 
         if self.autorizacionLider :
