69133ce04283eb0ab1ef743aaff665289ef5f8c8
cesar <kliko1989@gmail.com>
2020-08-03 02:06:21 -0500

    xdxdsxz

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -377,7 +377,7 @@ class StockCambioLine(TransientModel):
     #        ex2=self.env['stock.quant'].search([['location_id','=',41917],['product_id','=',record.producto1.id]]).sorted(key='quantity',reverse=True)
     #        record.existencia2=int(ex2[0].quantity) if(len(ex2)>0) else 0
     
-    @api.depends('producto1','almacen')
+    @api.depends('producto2','almacen')
     def almac(self):
         _logger.info('entre1')
         res={}
@@ -385,6 +385,12 @@ class StockCambioLine(TransientModel):
             if(record.almacen):
                 ex=self.env['stock.quant'].search([['location_id','=',record.almacen.lot_stock_id.id],['product_id','=',record.producto1.id]]).sorted(key='quantity',reverse=True)
                 record.existeciaAlmacen=int(ex[0].quantity) if(len(ex)>0) else 0
+        if(self.producto1.categ_id.id!=5):
+            res['domain']={'producto2':[['categ_id','=',self.producto1.categ_id.id]]}
+        if(self.producto1.categ_id.id==5):
+            p=self.env['product.product'].search([['categ_id','=',5],['name','ilike',self.producto1.name]])
+            res['domain']={'producto2':[['id','in',p.mapped('id')]]}
+        return res
     @api.onchange('existeciaAlmacen')
     def te(self):
         res={}
