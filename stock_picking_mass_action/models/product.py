c3fce8cdc983083644f93600f8adc703a26348ff
cesar <kliko1989@gmail.com>
2020-08-04 01:59:19 -0500

    dfgk,.-

--- stock_picking_mass_action/models/product.py
+++ stock_picking_mass_action/models/product.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api,_
+from odoo import models, fields, api,_,exceptions
 import base64
 from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
@@ -20,7 +20,7 @@ from xml.dom import minidom
 
 class compras(models.Model):
     _inherit = 'product.product'
-    
+
     @api.constrains('default_code')
     def noDuplicado(self):
         p=self.env['product.product'].search([['default_code','=',self.default_code]])
