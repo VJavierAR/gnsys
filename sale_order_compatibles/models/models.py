a8ae43a08f74e778cc854077feb3ed83f64d9fd7
Cesar <kliko1989@gmail.com>
2020-08-11 15:03:53 -0500

    fghjkl

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -84,9 +84,8 @@ class miniModeloAccesorio(models.Model):
 class sale_update(models.Model):
 	_inherit = 'sale.order'
 	compatiblesLineas = fields.One2many('sale_order_compatibles', 'saleOrder', string = 'nombre temp',copy=True)
-	serieRetiro=fields.Many2one('stock.production.lot','Serie')
 
-
+	serieRetiro2=fields.Many2one('stock.production.lot','Serie retiro')
 
 	@api.onchange('serieRetiro')
 	def serieRetiro(self):
