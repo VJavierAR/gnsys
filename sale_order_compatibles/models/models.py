234f5c4ef306632912cf876b5d32b94ba2e353b2
Cesar <kliko1989@gmail.com>
2020-08-18 15:17:53 -0500

    ghj

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -94,7 +94,7 @@ class sale_update(models.Model):
 			if(record.partner_id.id):
 				hijos=self.env['res.partner'].search([['parent_id','=',record.partner_id.id]])
 				hijosarr=hijos.mapped('id')
-				nietos=self.env['res.partner'].search([['parent_id','in',hijos],['type','=','contact']]).mapped('id')
+				nietos=self.env['res.partner'].search([['parent_id','in',hijosarr],['type','=','contact']]).mapped('id')
 				hijosF=hijos.filtered(lambda x:x.type=='contact').mapped('id')
 				final=nietos+hijosF
 				red['domain']={'x_studio_field_RnhKr':[('id','in',final)]}
