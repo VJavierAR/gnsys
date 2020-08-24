89a7c12a1249c7b79a0b8289a47b5d99e9cd6ed7
Cesar <kliko1989@gmail.com>
2020-08-24 15:13:51 -0500

    dfghjklñfghjkl

--- stock_picking_mass_action/report/report.py
+++ stock_picking_mass_action/report/report.py
@@ -244,7 +244,7 @@ class PartnerXlsx(models.AbstractModel):
         report_name = 'Tickets'
         bold = workbook.add_format({'bold': True})
         sheet = workbook.add_worksheet('Tickets')
-        sheet.merge_range('A1:R1', 'Tickets', merge_format)
+        sheet.merge_range('A1:S1', 'Tickets', merge_format)
         for obj in ticket:
             tipo=''
             if obj.x_studio_equipo_por_nmero_de_serie_1:
@@ -280,6 +280,7 @@ class PartnerXlsx(models.AbstractModel):
                 sheet.write(i, 15, obj.x_studio_tecnico if(obj.x_studio_tecnico) else obj.write_uid.name, bold)
                 sheet.write(i, 16, str(str(obj.x_studio_empresas_relacionadas.street_name)+" No. Ext. "+str(obj.x_studio_empresas_relacionadas.street_number)+" No. Int. "+str(obj.x_studio_empresas_relacionadas.street_number2)+" ,COL. "+str(obj.x_studio_empresas_relacionadas.l10n_mx_edi_colony)+" "+str(obj.x_studio_empresas_relacionadas.city)+" México, "+str(obj.x_studio_empresas_relacionadas.state_id.name)+"C.P "+str(obj.x_studio_empresas_relacionadas.zip)), bold)
                 sheet.write(i, 17, obj.x_studio_nmero_de_ticket_cliente if(obj.x_studio_nmero_de_ticket_cliente) else '', bold)
+                sheet.write(i, 18, obj.x_studio_nmero_de_guia_1 if(obj.x_studio_nmero_de_guia_1) else '', bold)
                 i=i+1
             if(len(obj.x_studio_equipo_por_nmero_de_serie_1)>1 or len(obj.x_studio_equipo_por_nmero_de_serie)>1):
                 series=None
@@ -289,17 +290,8 @@ class PartnerXlsx(models.AbstractModel):
                     a=True
                 if(len(obj.x_studio_equipo_por_nmero_de_serie)>1):
                     series=obj.x_studio_equipo_por_nmero_de_serie
+                p=[]
                 for s in series:
-                    sheet.write(i, 0, obj.x_studio_field_nO7Xg.warehouse_id.name if(obj.x_studio_field_nO7Xg.id) else '', bold)
-                    sheet.write(i, 1, obj.id, bold)
-                    sheet.write(i, 2, obj.x_studio_tipo_de_vale if(obj.x_studio_tipo_de_vale) else '', bold)
-                    sheet.write(i, 3, obj.create_date.strftime("%Y/%m/%d, %H:%M:%S"), bold)
-                    sheet.write(i, 4, obj.days_difference, bold)
-                    sheet.write(i, 5, obj.partner_id.name if(obj.partner_id) else '', bold)
-                    sheet.write(i, 6, obj.x_studio_empresas_relacionadas.name if(obj.x_studio_empresas_relacionadas) else '', bold)
-                    sheet.write(i, 7, str(s.serie.name) if(a) else str(s.name), bold)
-                    sheet.write(i, 8, str(s.serie.product_id.name) if(a) else str(s.name), bold)
-                    p=[]
                     if(a):
                         if(s.x_studio_cartuchonefro):
                             p.append(s.x_studio_cartuchonefro.name)
@@ -309,7 +301,17 @@ class PartnerXlsx(models.AbstractModel):
                             p.append(s.x_studio_cartucho_cian_1.name)
                         if(s.x_studio_cartucho_magenta):
                             p.append(s.x_studio_cartucho_magenta.name)
-                    sheet.write(i, 9, str(p).replace('[\'','').replace('\']','').replace('\'','') if(a) else str(obj.x_studio_productos.mapped('name')).replace('[\'','').replace('\']','').replace('\'',''), bold)
+                if(series!=None):
+                    sheet.write(i, 0, obj.x_studio_field_nO7Xg.warehouse_id.name if(obj.x_studio_field_nO7Xg.id) else '', bold)
+                    sheet.write(i, 1, obj.id, bold)
+                    sheet.write(i, 2, obj.x_studio_tipo_de_vale if(obj.x_studio_tipo_de_vale) else '', bold)
+                    sheet.write(i, 3, obj.create_date.strftime("%Y/%m/%d, %H:%M:%S"), bold)
+                    sheet.write(i, 4, obj.days_difference, bold)
+                    sheet.write(i, 5, obj.partner_id.name if(obj.partner_id) else '', bold)
+                    sheet.write(i, 6, obj.x_studio_empresas_relacionadas.name if(obj.x_studio_empresas_relacionadas) else '', bold)
+                    sheet.write(i, 7, str(series.mapped('serie.name')) if(a) else str(series.mapped('name')), bold)
+                    sheet.write(i, 8, str(series.mapped('serie.product_id.name')) if(a) else str(series.mapped('name')), bold)
+                    sheet.write(i, 9, str(p) if(a) else str(obj.x_studio_productos.mapped('name')), bold)
                     sheet.write(i, 10, obj.team_id.name if(obj.team_id.id) else "", bold)
                     sheet.write(i, 11,obj.x_studio_empresas_relacionadas.state_id.name if(obj.x_studio_empresas_relacionadas.state_id) else '' , bold)
                     sheet.write(i, 12, obj.stage_id.name if(obj.stage_id.id) else '', bold)
@@ -317,7 +319,7 @@ class PartnerXlsx(models.AbstractModel):
                     sheet.write(i, 14, obj.write_date.strftime("%Y/%m/%d, %H:%M:%S"), bold)
                     sheet.write(i, 15, obj.x_studio_tecnico if(obj.x_studio_tecnico) else obj.write_uid.name, bold)
                     sheet.write(i, 16, str(str(obj.x_studio_empresas_relacionadas.street_name)+" No. Ext. "+str(obj.x_studio_empresas_relacionadas.street_number)+" No. Int. "+str(obj.x_studio_empresas_relacionadas.street_number2)+" ,COL. "+str(obj.x_studio_empresas_relacionadas.l10n_mx_edi_colony)+" "+str(obj.x_studio_empresas_relacionadas.city)+" México, "+str(obj.x_studio_empresas_relacionadas.state_id.name)+"C.P "+str(obj.x_studio_empresas_relacionadas.zip)), bold)
-                    sheet.write(i, 17, obj.x_studio_nmero_de_ticket_cliente if(obj.x_studio_nmero_de_ticket_cliente) else '', bold)
+                    sheet.write(i, 18, obj.x_studio_nmero_de_guia_1 if(obj.x_studio_nmero_de_guia_1) else '', bold)
                     i=i+1
                 else:
                     sheet.write(i, 0, obj.x_studio_field_nO7Xg.warehouse_id.name if(obj.x_studio_field_nO7Xg.id) else '', bold)
@@ -338,7 +340,9 @@ class PartnerXlsx(models.AbstractModel):
                     sheet.write(i, 15, obj.x_studio_tecnico if(obj.x_studio_tecnico) else obj.write_uid.name, bold)
                     sheet.write(i, 16, str(str(obj.x_studio_empresas_relacionadas.street_name)+" No. Ext. "+str(obj.x_studio_empresas_relacionadas.street_number)+" No. Int. "+str(obj.x_studio_empresas_relacionadas.street_number2)+" ,COL. "+str(obj.x_studio_empresas_relacionadas.l10n_mx_edi_colony)+" "+str(obj.x_studio_empresas_relacionadas.city)+" México, "+str(obj.x_studio_empresas_relacionadas.state_id.name)+"C.P "+str(obj.x_studio_empresas_relacionadas.zip)), bold)
                     sheet.write(i, 17, obj.x_studio_nmero_de_ticket_cliente if(obj.x_studio_nmero_de_ticket_cliente) else '', bold)
-        sheet.add_table('A2:R'+str(i),{'columns': [{'header': 'Almacen'},{'header': 'Ticket'},{'header': 'Tipo de Reporte'},{'header': 'Fecha'},{'header':'Dias de atraso'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Serie'},{'header': 'Modelo'},{'header': 'Productos'},{'header': 'Area de Atención'},{'header': 'Zona'},{'header': 'Estado'},{'header': 'Ultima nota'},{'header': 'Fecha nota'},{'header': 'Tecnico'},{'header': 'Dirección'},{'header': 'No. Ticket cliente'}]}) 
+                    sheet.write(i, 18, obj.x_studio_nmero_de_guia_1 if(obj.x_studio_nmero_de_guia_1) else '', bold)
+                    i=i+1
+        sheet.add_table('A2:S'+str(i),{'style': 'Table Style Medium 9','columns': [{'header': 'Almacen'},{'header': 'Ticket'},{'header': 'Tipo de Reporte'},{'header': 'Fecha'},{'header':'Dias de atraso'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Serie'},{'header': 'Modelo'},{'header': 'Productos'},{'header': 'Area de Atención'},{'header': 'Zona'},{'header': 'Estado'},{'header': 'Ultima nota'},{'header': 'Fecha nota'},{'header': 'Tecnico'},{'header': 'Dirección'},{'header': 'No. Ticket cliente'},{'header':'Guia'}]}) 
         workbook.close()
 
 class PartnerXlsx(models.AbstractModel):
