6935cb12f47d1668bf7ce15be921dcf6a2de4a30
Marco Mandujano <47366939+VJavierAR@users.noreply.github.com>
2020-08-28 12:35:42 -0500

    Actualizacion
    Solucion en baklog

--- stock_picking_mass_action/report/report.py
+++ stock_picking_mass_action/report/report.py
@@ -342,6 +342,27 @@ class PartnerXlsx(models.AbstractModel):
                     sheet.write(i, 17, obj.x_studio_nmero_de_ticket_cliente if(obj.x_studio_nmero_de_ticket_cliente) else '', bold)
                     sheet.write(i, 18, obj.x_studio_nmero_de_guia_1 if(obj.x_studio_nmero_de_guia_1) else '', bold)
                     i=i+1
+            if(len(obj.x_studio_equipo_por_nmero_de_serie_1) == 0 or len(obj.x_studio_equipo_por_nmero_de_serie) == 0):
+                sheet.write(i, 0, obj.x_studio_field_nO7Xg.warehouse_id.name if(obj.x_studio_field_nO7Xg.id) else '', bold)
+                sheet.write(i, 1, obj.id, bold)
+                sheet.write(i, 2, obj.x_studio_tipo_de_vale if(obj.x_studio_tipo_de_vale) else '', bold)
+                sheet.write(i, 3, obj.create_date.strftime("%Y/%m/%d, %H:%M:%S"), bold)
+                sheet.write(i, 4, obj.days_difference, bold)
+                sheet.write(i, 5, obj.partner_id.name if(obj.partner_id) else '', bold)
+                sheet.write(i, 6, obj.x_studio_empresas_relacionadas.name if(obj.x_studio_empresas_relacionadas) else '', bold)
+                sheet.write(i, 7, '', bold)
+                sheet.write(i, 8, '', bold)
+                sheet.write(i, 9, '', bold)
+                sheet.write(i, 10, obj.team_id.name, bold)
+                sheet.write(i, 11, obj.x_studio_empresas_relacionadas.state_id.name if(obj.x_studio_empresas_relacionadas.state_id) else '' , bold)
+                sheet.write(i, 12, obj.stage_id.name if(obj.stage_id.id) else '', bold)
+                sheet.write(i, 13, obj.x_studio_ultima_nota if(obj.x_studio_ultima_nota) else '', bold)
+                sheet.write(i, 14, obj.write_date.strftime("%Y/%m/%d, %H:%M:%S"), bold)
+                sheet.write(i, 15, obj.x_studio_tecnico if(obj.x_studio_tecnico) else obj.write_uid.name, bold)
+                sheet.write(i, 16, str(str(obj.x_studio_empresas_relacionadas.street_name)+" No. Ext. "+str(obj.x_studio_empresas_relacionadas.street_number)+" No. Int. "+str(obj.x_studio_empresas_relacionadas.street_number2)+" ,COL. "+str(obj.x_studio_empresas_relacionadas.l10n_mx_edi_colony)+" "+str(obj.x_studio_empresas_relacionadas.city)+" México, "+str(obj.x_studio_empresas_relacionadas.state_id.name)+"C.P "+str(obj.x_studio_empresas_relacionadas.zip)), bold)
+                sheet.write(i, 17, obj.x_studio_nmero_de_ticket_cliente if(obj.x_studio_nmero_de_ticket_cliente) else '', bold)
+                sheet.write(i, 18, obj.x_studio_nmero_de_guia_1 if(obj.x_studio_nmero_de_guia_1) else '', bold)
+                i=i+1
         sheet.add_table('A2:S'+str(i),{'style': 'Table Style Medium 9','columns': [{'header': 'Almacen'},{'header': 'Ticket'},{'header': 'Tipo de Reporte'},{'header': 'Fecha'},{'header':'Dias de atraso'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Serie'},{'header': 'Modelo'},{'header': 'Productos'},{'header': 'Area de Atención'},{'header': 'Zona'},{'header': 'Estado'},{'header': 'Ultima nota'},{'header': 'Fecha nota'},{'header': 'Tecnico'},{'header': 'Dirección'},{'header': 'No. Ticket cliente'},{'header':'Guia'}]}) 
         workbook.close()
 
