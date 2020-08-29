3a1f95bc291046730129916b6b45f2a773c7ffd9
Cesar <kliko1989@hotmail.com>
2020-08-29 13:16:23 -0500

    dfghjkl

--- stock_picking_mass_action/report/report.py
+++ stock_picking_mass_action/report/report.py
@@ -460,7 +460,7 @@ class RutaXlsx(models.AbstractModel):
         report_name = 'Expedición'
         bold = workbook.add_format({'bold': True})
         sheet = workbook.add_worksheet('Expedición')
-        sheet.merge_range('A1:T1', 'Reporte Expedición', merge_format)
+        sheet.merge_range('A1:V1', 'Reporte Expedición', merge_format)
         for obj in ruta:
             for orden in obj.ordenes:
                 sheet.write(i, 0, obj.name if(obj.name) else '', bold)
@@ -483,6 +483,8 @@ class RutaXlsx(models.AbstractModel):
                 sheet.write(i, 17, orden.partner_id.l10n_mx_edi_locality if(orden.partner_id.l10n_mx_edi_locality) else '', bold)
                 sheet.write(i, 18, orden.partner_id.city if(orden.partner_id.city) else '', bold)
                 sheet.write(i, 19, orden.partner_id.zip if(orden.partner_id.zip) else '', bold)
+                sheet.write(i, 20, orden.x_studio_field_RnhKr.name if(orden.x_studio_field_RnhKr.name) else '', bold)
+                sheet.write(i, 21, orden.x_studio_field_RnhKr.phone if(orden.x_studio_field_RnhKr.phone) else '', bold)
                 i=i+1
-        sheet.add_table('A2:T'+str(i),{'columns': [{'header': 'Expedición'},{'header': 'Fecha'},{'header': 'chofer'},{'header': 'vehiculo'},{'header': 'Zona'},{'header':'Tipo'},{'header': 'Estado'},{'header': 'Ticket'},{'header': 'Orden'},{'header': 'Cantidad'},{'header': 'Productos'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Calle'},{'header': 'No exterior'},{'header': 'No Interior'},{'header': 'Colonia'},{'header': 'Delegación'},{'header': 'Ciudad'},{'header': 'C.p'}]}) 
+        sheet.add_table('A2:V'+str(i),{'columns': [{'header': 'Expedición'},{'header': 'Fecha'},{'header': 'chofer'},{'header': 'vehiculo'},{'header': 'Zona'},{'header':'Tipo'},{'header': 'Estado'},{'header': 'Ticket'},{'header': 'Orden'},{'header': 'Cantidad'},{'header': 'Productos'},{'header': 'Cliente'},{'header': 'Localidad'},{'header': 'Calle'},{'header': 'No exterior'},{'header': 'No Interior'},{'header': 'Colonia'},{'header': 'Delegación'},{'header': 'Ciudad'},{'header': 'C.p'},{'header': 'Contacto'},{'header': 'Telefono'}]}) 
         workbook.close()  
