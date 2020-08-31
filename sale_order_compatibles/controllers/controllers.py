8fdcb38194c43c6d1d348eeb17ca53aec42307fc
Cesar <kliko1989@gmail.com>
2020-08-31 10:53:46 -0500

    sdfgds

--- sale_order_compatibles/controllers/controllers.py
+++ sale_order_compatibles/controllers/controllers.py
@@ -21,10 +21,10 @@ class SaleOrderCompatibles(http.Controller):
             p.retiro()
         if(u.id==False):
             return "No tiene permisos para realizar esta acción"
-        name = 'Sale'
-        res_model = 'sale.order' 
-        view_name = 'studio_customization.sale_order_form_8690a815-6188-42ab-9845-1c18a02ee045'
-        view = self.env.ref(view_name)
+        #name = 'Sale'
+        #res_model = 'sale.order' 
+        #view_name = 'studio_customization.sale_order_form_8690a815-6188-42ab-9845-1c18a02ee045'
+        #view = self.env.ref(view_name)
         return request.redirect('/web#id='+str(sale_id)+'&action=606&model=sale.order&view_type=form&menu_id=406')  
 
 
