e6c07c15ae2ccb917b12c573ed062f3700e03d52
cesar <kliko1989@gmail.com>
2020-11-20 03:09:44 -0600

    dededsde

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -128,11 +128,12 @@ class sale_update(models.Model):
 	    pos[0].write({'x_studio_arreglo':str(pos.mapped('id'))})
 	    template_id2=self.env['mail.template'].search([('id','=',79)], limit=1)
 	    mail=template_id2.generate_email(pos[0].id)
-	    pdf=self.env.ref('stock_picking_mass_action.sale_xlsx').sudo().render_xlsx(data=pos[0],docids=pos[0].id)[0]
-	    reporte = base64.encodestring(pdf)
-	    at=self.env['ir.attachment'].create({'name':'Reporte Demostración y prestamos','datas':reporte})
-	    mail.write({'attachment_ids':[(6,0,[at.id])]})
-	    mail.send()
+	    _logger.info(str(mail))
+	    #pdf=self.env.ref('stock_picking_mass_action.sale_xlsx').sudo().render_xlsx(data=pos[0],docids=pos[0].id)[0]
+	    #reporte = base64.encodestring(pdf)
+	    #at=self.env['ir.attachment'].create({'name':'Reporte Demostración y prestamos','datas':reporte})
+	    #mail.write({'attachment_ids':[(6,0,[at.id])]})
+	    #mail.send()
 
 
 	@api.onchange('partner_id')
