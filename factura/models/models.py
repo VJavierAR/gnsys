ea092b482dd262abfe4e8187bd5359d28ece262a
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2021-01-08 04:15:28 -0600

    asasas

--- factura/models/models.py
+++ factura/models/models.py
@@ -10,7 +10,14 @@ import pytz
 from odoo import http
 from odoo.addons.web.controllers.main import ReportController  # Import the class
 """
-
+def get_years():
+    year_list = []
+    for i in range(2010, 2036):
+       year_list.append((i, str(i)))
+    return year_list
+valores = [('01', 'Enero'), ('02', 'Febrero'), ('03', 'Marzo'), ('04', 'Abril'),
+                          ('05', 'Mayo'), ('06', 'Junio'), ('07', 'Julio'), ('08', 'Agosto'),
+                          ('09', 'Septiembre'), ('10', 'Octubre'), ('11', 'Noviembre'), ('12', 'Diciembre')]
 
 
 
