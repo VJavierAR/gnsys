0ab6c8531a17d016ff0f5f967dbae5c4fc617d80
Cesar <kliko1989@hotmail.com>
2021-02-10 18:22:56 -0600

    sjkdflg

--- res_user/models/models.py
+++ res_user/models/models.py
@@ -1,27 +1,25 @@
 from odoo import models
 import base64
+from odoo import api, fields, models, _, SUPERUSER_ID
 
 class ResUser(models.Model):
-    _inherit = "res.user"
+    _inherit = "res.users"
     second_users=fields.One2many('second.user','rel_user')
 
 class SecondUser(models.Model):
-	rel_user=fields.Many2one('res.user')
-	name=fields.Char()
-	user=fields.Char()
-	password=fields.Char(string='password')
-
-	@api.model
+    _name='second.user'
+    rel_user=fields.Many2one('res.user')
+    name=fields.Char('Nombre')
+    user=fields.Char('Mail')
+    password=fields.Char('Password')
+    
+    @api.model
     def create(self, vals):
-    	password=vals['password']
-    	password_bytes = password.encode('ascii')
-    	base64_bytes = base64.b64encode(password_bytes)
-    	base64_message = base64_bytes.decode('ascii')
+        password=vals['password']
+        password_bytes = password.encode('ascii')
+        base64_bytes = base64.b64encode(password_bytes)
+        base64_message = base64_bytes.decode('ascii')
         vals['password'] = base64_message
         result = super(SecondUser, self).create(vals)
         return result
-
-
-
-
 		
