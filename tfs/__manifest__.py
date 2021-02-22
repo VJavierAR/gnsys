45ed80baa863d4cacd0a10bc88a976ace3633e9c
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2021-02-22 01:21:42 -0600

    xassxsasx

--- tfs/__manifest__.py
+++ tfs/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Long descrifdhghssdjkdjkdskjption of module's purpose
     """,
 
     'author': "My Company",
